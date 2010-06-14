b9873594ed4b6b863f4ccb5beab7bd01120d31d2
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2010-06-14 11:39:47 +0200

    ExtMvc

--- dotnet/ExtMvc/js/ExtMvc.OrderNormalSearchPanel.js
+++ dotnet/ExtMvc/js/ExtMvc.OrderNormalSearchPanel.js
@@ -1,4 +1,4 @@
-/*jslint white: true, browser: true, devel: true, onevar: true, undef: true, nomen: true, eqeqeq: true, plusplus: true, bitwise: true, regexp: true, strict: true, newcap: true, immed: true */
+/*jslint white: true, onevar: true, browser: true, devel: true, undef: true, eqeqeq: true, plusplus: true, bitwise: true, regexp: true, strict: true, newcap: true, immed: true */
 /*global Ext, Rpc, ExtMvc */
 "use strict";
 
@@ -8,32 +8,11 @@ ExtMvc.OrderNormalSearchPanel = Ext.extend(Ext.Panel, {
 	layout: 'border',
 	border: false,
 	initComponent: function () {
-		var store = new Ext.data.Store({
-			autoDestroy: true,
-			proxy: new Rpc.JsonPostHttpProxy({
-				url: '/Order/SearchNormal'
-			}),
-			remoteSort: true,
-			reader: new ExtMvc.OrderJsonReader()
-		});
-		this.gridPanel = new ExtMvc.OrderGridPanel({
-			region: 'center',
-			store: store,
-			bbar: new Ext.PagingToolbar({
-				store: store,
-				displayInfo: true,
-				pageSize: 25,
-				prependButtons: true
-			}),
-			listeners: {
-				rowdblclick: {
-					fn: this.gridPanel_rowDblClick,
-					scope: this
-				}
-			}
-		});
-
-		this.searchFormPanel = new ExtMvc.OrderNormalSearchFormPanel({
+		var _onGridPanelRowDblClick = function (grid, rowIndex, event) {
+			var item = grid.getStore().getAt(rowIndex).data;
+			this.fireEvent('itemselected', this, item);
+		},
+		_searchFormPanel = new ExtMvc.OrderNormalSearchFormPanel({
 			title: 'Search Filters',
 			region: 'north',
 			autoHeight: true,
@@ -41,52 +20,65 @@ ExtMvc.OrderNormalSearchPanel = Ext.extend(Ext.Panel, {
 			collapsed: true,
 			titleCollapse: true,
 			floatable: false
-		});
+		}),
+		_store = new Ext.data.Store({
+			autoDestroy: true,
+			proxy: new Rpc.JsonPostHttpProxy({
+				url: '/Order/SearchNormal'
+			}),
+			remoteSort: true,
+			reader: new ExtMvc.OrderJsonReader()
+		}),
+		_gridPanel = new ExtMvc.OrderGridPanel({
+			region: 'center',
+			store: _store,
+			bbar: new Ext.PagingToolbar({
+				store: _store,
+				displayInfo: true,
+				pageSize: 25,
+				prependButtons: true
+			}),
+			listeners: {
+				rowdblclick: _onGridPanelRowDblClick,
+				scope: this
+			}
+		}),
+		_onSearchButtonClick = function (b, e) {
+			var params = _searchFormPanel.getForm().getFieldValues();
+			Ext.apply(_gridPanel.getStore().baseParams, params);
+			_gridPanel.getStore().load({
+				params: {
+					start: 0,
+					limit: _gridPanel.getBottomToolbar().pageSize
+				}
+			});
+		},
+		_onNewButtonClick = function () {
+			alert('onNewButtonClick');
+		},
+		_onEditButtonClick = function () {
+			alert('onEditButtonClick');
+		},
+		_onDeleteButtonClick = function () {
+			alert('onDeleteButtonClick');
+		};
 
-		this.items = [this.searchFormPanel, this.gridPanel];
+		this.items = [_searchFormPanel, _gridPanel];
 
 		this.tbar = [
-			{ text: 'Search', handler: this.onSearchButtonClick, icon: '/images/zoom.png', cls: 'x-btn-text-icon', scope: this },
-			{ text: 'New', handler: this.onNewButtonClick, icon: '/images/add.png', cls: 'x-btn-text-icon', scope: this },
-			{ text: 'Edit', handler: this.onEditButtonClick, icon: '/images/pencil.png', cls: 'x-btn-text-icon', scope: this },
-			{ text: 'Delete', handler: this.onDeleteButtonClick, icon: '/images/delete.png', cls: 'x-btn-text-icon', scope: this }
+			{ text: 'Search', handler: _onSearchButtonClick, icon: '/images/zoom.png', cls: 'x-btn-text-icon', scope: this },
+			{ text: 'New', handler: _onNewButtonClick, icon: '/images/add.png', cls: 'x-btn-text-icon', scope: this },
+			{ text: 'Edit', handler: _onEditButtonClick, icon: '/images/pencil.png', cls: 'x-btn-text-icon', scope: this },
+			{ text: 'Delete', handler: _onDeleteButtonClick, icon: '/images/delete.png', cls: 'x-btn-text-icon', scope: this }
 		];
+
+		this.getSelectedItem = function () {
+			var sm = _gridPanel.getSelectionModel();
+			return sm.getCount() > 0 ? sm.getSelected().data : null;
+		};
 
 		this.addEvents('itemselected');
 
 		ExtMvc.OrderNormalSearchPanel.superclass.initComponent.apply(this, arguments);
-	},
-
-	gridPanel_rowDblClick: function (grid, rowIndex, event) {
-		var item = grid.getStore().getAt(rowIndex).data;
-		this.fireEvent('itemselected', this, item);
-	},
-
-	getSelectedItem: function () {
-		var sm = this.gridPanel.getSelectionModel();
-		return sm.getCount() > 0 ? sm.getSelected().data : null;
-	},
-
-	onSearchButtonClick: function (b, e) {
-		var params = this.searchFormPanel.getForm().getFieldValues();
-		Ext.apply(this.gridPanel.getStore().baseParams, params);
-		this.gridPanel.getStore().load({
-			params: {
-				start: 0,
-				limit: this.gridPanel.getBottomToolbar().pageSize
-			}
-		});
-	},
-
-	onNewButtonClick: function () {
-		alert('onNewButtonClick');
-	},
-
-	onEditButtonClick: function () {
-		alert('onEditButtonClick');
-	},
-
-	onDeleteButtonClick: function () {
-		alert('onDeleteButtonClick');
 	}
 });
