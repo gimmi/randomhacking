c240dba0fb6b0546a6c9064ba711a39664dde694
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2010-07-23 15:43:52 +0200

    spikeAppEngine

--- java/spikeAppEngine/spikeAppEngine/src/spikeAppEngine/SpikeAppEngineServlet.java
+++ java/spikeAppEngine/spikeAppEngine/src/spikeAppEngine/SpikeAppEngineServlet.java
@@ -9,9 +9,12 @@ import javax.servlet.http.*;
 public class SpikeAppEngineServlet extends HttpServlet {
 	private static final Logger log = Logger.getLogger(SpikeAppEngineServlet.class.getName());
 
+	private static org.apache.log4j.Logger log4j = org.apache.log4j.Logger.getLogger(SpikeAppEngineServlet.class);
+
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		resp.setContentType("text/plain");
 		resp.getWriter().println("Hello, world");
 		log.severe("Severe logging message just to test logging configuration");
+		log4j.error("test log4j message");
 	}
 }
