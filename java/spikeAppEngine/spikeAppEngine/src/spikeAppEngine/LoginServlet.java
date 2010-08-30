df09e90d3d6984dbd818bb9e2fc22dd59927575f
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2010-08-30 10:11:06 +0200

    spikeAppEngine

---
+++ java/spikeAppEngine/spikeAppEngine/src/spikeAppEngine/LoginServlet.java
@@ -0,0 +1,16 @@
+package spikeAppEngine;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
