d45d366108317e68fcc1cd10f69be85e48c1a8d7
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2010-12-18 21:04:53 +0100

    spike-dwr

--- java/spike-dwr/src/main/java/com/github/gimmi/spikedwr/RemoteMethods.java
+++ java/spike-dwr/src/main/java/com/github/gimmi/spikedwr/RemoteMethods.java
@@ -1,6 +1,10 @@
 package com.github.gimmi.spikedwr;
 
+import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,4 +20,24 @@ public class RemoteMethods {
 		logger.info("date={}", date);
 		return date;
 	}
+
+	public Map<Integer, String> mapWithIntegerKeys(Map<Integer, String> map) {
+		map.put(123, "from java");
+		return map;
+	}
+
+	public List<Map<Integer, Date>> listOfMap(List<Map<Integer, Date>> list) {
+		Map<Integer, Date> map = new HashMap<Integer, Date>();
+		map.put(123, newDate(2010, 12, 23));
+		list.add(map);
+		return list;
+	}
+
+	private static Date newDate(int year, int month, int day) {
+		Calendar calendar = Calendar.getInstance();
+		calendar.clear();
+		calendar.set(year, month - 1, day, 0, 0, 0);
+		return calendar.getTime();
+	}
+
 }
--- java/spike-dwr/src/main/webapp/js/tests.js
+++ java/spike-dwr/src/main/webapp/js/tests.js
@@ -10,17 +10,17 @@ describe('RemoteMethods', function () {
 			retVal = ret;
 			callCompleted = true;
 		});
-		
+
 		waitsFor(function () {
 			return callCompleted;
 		}, 'Server call', 1000);
-		
+
 		runs(function () {
 			expect(retVal).toEqual('123, str, true, Sat Dec 25 00:00:00 CET 2010, 3.14');
 		});
 
 	});
-	
+
 	it('dateEcho', function () {
 		var callCompleted = false;
 		var retVal;
@@ -28,15 +28,72 @@ describe('RemoteMethods', function () {
 			retVal = ret;
 			callCompleted = true;
 		});
-		
+
 		waitsFor(function () {
 			return callCompleted;
 		}, 'Server call', 1000);
-		
+
 		runs(function () {
 			expect(retVal).toEqual(new Date('2010-12-25'));
 			expect(retVal instanceof Date).toBeTruthy();
 		});
-		
+
+	});
+
+	it('mapWithIntegerKeys', function () {
+		var callCompleted = false;
+		var retVal;
+		RemoteMethods.mapWithIntegerKeys({
+			1: 'one',
+			2: 'two'
+		}, function (ret) {
+			retVal = ret;
+			callCompleted = true;
+		});
+
+		waitsFor(function () {
+			return callCompleted;
+		}, 'Server call', 1000);
+
+		runs(function () {
+			expect(retVal).toEqual({
+				1: 'one',
+				2: 'two',
+				123: 'from java'
+			});
+		});
+
+	});
+
+	it('listOfMap', function () {
+		var callCompleted = false;
+		var retVal;
+		RemoteMethods.listOfMap([ {
+			1: new Date('2010-12-01'),
+			2: new Date('2010-12-02')
+		}, {
+			3: new Date('2010-12-03'),
+			4: new Date('2010-12-04')
+		} ], function (ret) {
+			retVal = ret;
+			callCompleted = true;
+		});
+
+		waitsFor(function () {
+			return callCompleted;
+		}, 'Server call', 1000);
+
+		runs(function () {
+			expect(retVal).toEqual([ {
+				1: new Date('2010-12-01'),
+				2: new Date('2010-12-02')
+			}, {
+				3: new Date('2010-12-03'),
+				4: new Date('2010-12-04')
+			}, {
+				123: new Date('2010-12-23')
+			} ]);
+		});
+
 	});
 });
