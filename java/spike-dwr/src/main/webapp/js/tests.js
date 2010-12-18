2c3f7ce44579c62fe2ad9303a6a0b80aae893867
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2010-12-18 20:30:12 +0100

    spike-dwr

--- java/spike-dwr/src/main/webapp/js/tests.js
+++ java/spike-dwr/src/main/webapp/js/tests.js
@@ -3,7 +3,7 @@
 "use strict";
 
 describe('RemoteMethods', function () {
-	it('Should call rpc methods', function () {
+	it('simpleParametersEcho', function () {
 		var callCompleted = false;
 		var retVal;
 		RemoteMethods.simpleParametersEcho(123, 'str', true, new Date('2010-12-25'), 3.14, function (ret) {
@@ -19,5 +19,24 @@ describe('RemoteMethods', function () {
 			expect(retVal).toEqual('123, str, true, Sat Dec 25 00:00:00 CET 2010, 3.14');
 		});
 
+	});
+	
+	it('dateEcho', function () {
+		var callCompleted = false;
+		var retVal;
+		RemoteMethods.dateEcho(new Date('2010-12-25'), function (ret) {
+			retVal = ret;
+			callCompleted = true;
+		});
+		
+		waitsFor(function () {
+			return callCompleted;
+		}, 'Server call', 1000);
+		
+		runs(function () {
+			expect(retVal).toEqual(new Date('2010-12-25'));
+			expect(retVal instanceof Date).toBeTruthy();
+		});
+		
 	});
 });
