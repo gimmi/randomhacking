4746bab7d2c4c305688d1449f210601a2babf135
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2011-02-26 18:38:35 +0100

    spike-maven-gwt

--- java/spike-maven-gwt/src/main/java/com/github/gimmi/spikemavengwt/client/Spikemavengwt.java
+++ java/spike-maven-gwt/src/main/java/com/github/gimmi/spikemavengwt/client/Spikemavengwt.java
@@ -1,15 +1,24 @@
 package com.github.gimmi.spikemavengwt.client;
 
+import java.util.Arrays;
+import java.util.List;
+
+import com.google.gwt.cell.client.TextCell;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.user.cellview.client.CellList;
+import com.google.gwt.user.cellview.client.CellTable;
+import com.google.gwt.user.cellview.client.TextColumn;
 import com.google.gwt.user.client.Command;
+import com.google.gwt.user.client.Timer;
 import com.google.gwt.user.client.Window;
-import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.LayoutPanel;
 import com.google.gwt.user.client.ui.RootLayoutPanel;
 import com.google.gwt.user.client.ui.TabLayoutPanel;
+import com.google.gwt.view.client.AsyncDataProvider;
+import com.google.gwt.view.client.HasData;
+import com.google.gwt.view.client.Range;
 
 public class Spikemavengwt implements EntryPoint {
 	private final GreetingServiceAsync greetingService = GWT.create(GreetingService.class);
@@ -22,23 +31,98 @@ public class Spikemavengwt implements EntryPoint {
 				Window.alert("You selected a menu item!");
 			}
 		};
-		RootLayoutPanel rootLayoutPanel = RootLayoutPanel.get();
 
 		TabLayoutPanel tabLayoutPanel = new TabLayoutPanel(1.5, Unit.EM);
 
-		LayoutPanel layoutPanel = new LayoutPanel();
-		tabLayoutPanel.add(layoutPanel, "New Widget", false);
+		tabLayoutPanel.add(buildListTab(), "List", false);
+		tabLayoutPanel.add(buildTableTab(), "Table", false);
+		tabLayoutPanel.add(buildAsyncListTab(), "Async List", false);
 
-		Button btnNewButton = new Button("New button");
-		layoutPanel.add(btnNewButton);
-		layoutPanel.setWidgetLeftRight(btnNewButton, 10.0, Unit.EM, 10.0, Unit.EM);
-		layoutPanel.setWidgetTopBottom(btnNewButton, 10.0, Unit.EM, 10.0, Unit.EM);
+		RootLayoutPanel rootLayoutPanel = RootLayoutPanel.get();
 		rootLayoutPanel.add(tabLayoutPanel);
+		rootLayoutPanel.setWidgetLeftWidth(tabLayoutPanel, 0.0, Unit.PX, 100.0, Unit.PCT);
+		rootLayoutPanel.setWidgetTopHeight(tabLayoutPanel, 0.0, Unit.PX, 100.0, Unit.PCT);
+	}
 
-		HTML htmlCiao = new HTML("Ciao", true);
-		tabLayoutPanel.add(htmlCiao, "New Widget", false);
+	private LayoutPanel buildListTab() {
+		TextCell textCell = new TextCell();
+		CellList<String> cellList = new CellList<String>(textCell);
 
-		HTML htmlCiao_1 = new HTML("Ciao 2", true);
-		tabLayoutPanel.add(htmlCiao_1, "New Widget", false);
+		cellList.setRowCount(7, true);
+		cellList.setRowData(0, Arrays.asList("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"));
+
+		LayoutPanel layoutPanel = new LayoutPanel();
+		layoutPanel.add(cellList);
+		layoutPanel.setWidgetLeftWidth(cellList, 10.0, Unit.EM, 20.0, Unit.EM);
+		layoutPanel.setWidgetTopHeight(cellList, 10.0, Unit.EM, 20.0, Unit.EM);
+		return layoutPanel;
+	}
+
+	private LayoutPanel buildAsyncListTab() {
+		TextCell textCell = new TextCell();
+		final CellList<String> cellList = new CellList<String>(textCell);
+
+		final List<String> data = Arrays.asList("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
+		cellList.setVisibleRange(1, 3);
+		cellList.setRowCount(data.size()); // Should be asyncronously asked to the server
+
+		AsyncDataProvider<String> dataProvider = new AsyncDataProvider<String>() {
+			@Override
+			protected void onRangeChanged(final HasData<String> display) {
+				new Timer() {
+					@Override
+					public void run() {
+						// On the server...
+						Range range = display.getVisibleRange();
+						List<String> values = data.subList(range.getStart(), range.getStart() + range.getLength());
+						// On the callback...
+						display.setRowData(range.getStart(), values);
+					}
+				}.schedule(2000);
+			}
+		};
+		dataProvider.addDataDisplay(cellList);
+
+		LayoutPanel layoutPanel = new LayoutPanel();
+		layoutPanel.add(cellList);
+		layoutPanel.setWidgetLeftWidth(cellList, 10.0, Unit.EM, 20.0, Unit.EM);
+		layoutPanel.setWidgetTopHeight(cellList, 10.0, Unit.EM, 20.0, Unit.EM);
+		return layoutPanel;
+	}
+
+	private LayoutPanel buildTableTab() {
+		CellTable<Contact> table = new CellTable<Contact>();
+		table.addColumn(new TextColumn<Contact>() {
+			@Override
+			public String getValue(Contact contact) {
+				return contact.name;
+			}
+		}, "Name");
+		table.addColumn(new TextColumn<Contact>() {
+			@Override
+			public String getValue(Contact contact) {
+				return contact.address;
+			}
+		}, "Address");
+
+		List<Contact> contacts = Arrays.asList(new Contact("John", "123 Fourth Road"), new Contact("Mary", "222 Lancer Lane"));
+		table.setRowCount(contacts.size(), true);
+		table.setRowData(0, contacts);
+
+		LayoutPanel layoutPanel = new LayoutPanel();
+		layoutPanel.add(table);
+		layoutPanel.setWidgetLeftWidth(table, 10.0, Unit.EM, 20.0, Unit.EM);
+		layoutPanel.setWidgetTopHeight(table, 10.0, Unit.EM, 20.0, Unit.EM);
+		return layoutPanel;
+	}
+
+	private static class Contact {
+		private final String address;
+		private final String name;
+
+		public Contact(String name, String address) {
+			this.name = name;
+			this.address = address;
+		}
 	}
 }
