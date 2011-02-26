65be7b71665067762f5e32d0b89754b4ae5b2e45
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2011-02-26 11:56:36 +0100

    spike-maven-gwt

--- java/spike-maven-gwt/src/main/java/com/github/gimmi/spikemavengwt/client/Spikemavengwt.java
+++ java/spike-maven-gwt/src/main/java/com/github/gimmi/spikemavengwt/client/Spikemavengwt.java
@@ -1,139 +1,52 @@
 package com.github.gimmi.spikemavengwt.client;
 
-import com.github.gimmi.spikemavengwt.shared.FieldVerifier;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.event.dom.client.KeyCodes;
-import com.google.gwt.event.dom.client.KeyUpEvent;
-import com.google.gwt.event.dom.client.KeyUpHandler;
-import com.google.gwt.user.client.rpc.AsyncCallback;
-import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.DialogBox;
-import com.google.gwt.user.client.ui.HTML;
-import com.google.gwt.user.client.ui.Label;
-import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.TextBox;
-import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.user.client.Command;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.MenuBar;
+import com.google.gwt.user.client.ui.RootLayoutPanel;
 
 public class Spikemavengwt implements EntryPoint {
-	private static final String SERVER_ERROR = "An error occurred while "
-			+ "attempting to contact the server. Please check your network "
-			+ "connection and try again.";
 	private final GreetingServiceAsync greetingService = GWT.create(GreetingService.class);
 
 	@Override
 	public void onModuleLoad() {
-		final Button sendButton = new Button("Send");
-		final TextBox nameField = new TextBox();
-		nameField.setText("GWT User");
-		final Label errorLabel = new Label();
-
-		// We can add style names to widgets
-		sendButton.addStyleName("sendButton");
-
-		// Add the nameField and sendButton to the RootPanel
-		// Use RootPanel.get() to get the entire body element
-		RootPanel.get("nameFieldContainer").add(nameField);
-		RootPanel.get("sendButtonContainer").add(sendButton);
-		RootPanel.get("errorLabelContainer").add(errorLabel);
-
-		// Focus the cursor on the name field when the app loads
-		nameField.setFocus(true);
-		nameField.selectAll();
-
-		// Create the popup dialog box
-		final DialogBox dialogBox = new DialogBox();
-		dialogBox.setText("Remote Procedure Call");
-		dialogBox.setAnimationEnabled(true);
-		final Button closeButton = new Button("Close");
-		// We can set the id of a widget by accessing its Element
-		closeButton.getElement().setId("closeButton");
-		final Label textToServerLabel = new Label();
-		final HTML serverResponseLabel = new HTML();
-		VerticalPanel dialogVPanel = new VerticalPanel();
-		dialogVPanel.addStyleName("dialogVPanel");
-		dialogVPanel.add(new HTML("<b>Sending name to the server:</b>"));
-		dialogVPanel.add(textToServerLabel);
-		dialogVPanel.add(new HTML("<br><b>Server replies:</b>"));
-		dialogVPanel.add(serverResponseLabel);
-		dialogVPanel.setHorizontalAlignment(VerticalPanel.ALIGN_RIGHT);
-		dialogVPanel.add(closeButton);
-		dialogBox.setWidget(dialogVPanel);
-
-		// Add a handler to close the DialogBox
-		closeButton.addClickHandler(new ClickHandler() {
+		Command cmd = new Command() {
 			@Override
-			public void onClick(ClickEvent event) {
-				dialogBox.hide();
-				sendButton.setEnabled(true);
-				sendButton.setFocus(true);
+			public void execute() {
+				Window.alert("You selected a menu item!");
 			}
-		});
+		};
+		RootLayoutPanel rootLayoutPanel = RootLayoutPanel.get();
 
-		// Create a handler for the sendButton and nameField
-		class MyHandler implements ClickHandler, KeyUpHandler {
-			/**
-			 * Fired when the user clicks on the sendButton.
-			 */
-			@Override
-			public void onClick(ClickEvent event) {
-				sendNameToServer();
-			}
+		MenuBar fooMenu = new MenuBar(true);
+		fooMenu.addItem("the", cmd);
+		fooMenu.addItem("foo", cmd);
+		fooMenu.addItem("menu", cmd);
 
-			/**
-			 * Fired when the user types in the nameField.
-			 */
-			@Override
-			public void onKeyUp(KeyUpEvent event) {
-				if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
-					sendNameToServer();
-				}
-			}
+		MenuBar barMenu = new MenuBar(true);
+		barMenu.addItem("the", cmd);
+		barMenu.addItem("bar", cmd);
+		barMenu.addItem("menu", cmd);
 
-			/**
-			 * Send the name from the nameField to the server and wait for a response.
-			 */
-			private void sendNameToServer() {
-				// First, we validate the input.
-				errorLabel.setText("");
-				String textToServer = nameField.getText();
-				if (!FieldVerifier.isValidName(textToServer)) {
-					errorLabel.setText("Please enter at least four characters");
-					return;
-				}
+		MenuBar bazMenu = new MenuBar(true);
+		bazMenu.addItem("the", cmd);
+		bazMenu.addItem("baz", cmd);
+		bazMenu.addItem("menu", cmd);
 
-				// Then, we send the input to the server.
-				sendButton.setEnabled(false);
-				textToServerLabel.setText(textToServer);
-				serverResponseLabel.setText("");
-				greetingService.greetServer(textToServer, new AsyncCallback<String>() {
-					@Override
-					public void onFailure(Throwable caught) {
-						// Show the RPC error message to the user
-						dialogBox.setText("Remote Procedure Call - Failure");
-						serverResponseLabel.addStyleName("serverResponseLabelError");
-						serverResponseLabel.setHTML(SERVER_ERROR);
-						dialogBox.center();
-						closeButton.setFocus(true);
-					}
+		MenuBar menu = new MenuBar();
+		menu.addItem("foo", fooMenu);
+		menu.addItem("bar", barMenu);
+		menu.addItem("baz", bazMenu);
 
-					@Override
-					public void onSuccess(String result) {
-						dialogBox.setText("Remote Procedure Call");
-						serverResponseLabel.removeStyleName("serverResponseLabelError");
-						serverResponseLabel.setHTML(result);
-						dialogBox.center();
-						closeButton.setFocus(true);
-					}
-				});
-			}
-		}
+		FlowPanel flowPanel = new FlowPanel();
+		rootLayoutPanel.add(flowPanel);
+		rootLayoutPanel.setWidgetLeftWidth(flowPanel, 0.0, Unit.EM, 100.0, Unit.PCT);
+		rootLayoutPanel.setWidgetTopHeight(flowPanel, 0.0, Unit.EM, 100.0, Unit.PCT);
 
-		// Add a handler to send the name to the server
-		MyHandler handler = new MyHandler();
-		sendButton.addClickHandler(handler);
-		nameField.addKeyUpHandler(handler);
+		flowPanel.add(menu);
 	}
 }
