1848e574b849bba7a87299c967174faa0355e368
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2011-04-29 19:14:10 +0200

    spike-extjs4

--- java/spike-extjs4/src/main/java/com/github/gimmi/spikeextjs4/Service.java
+++ java/spike-extjs4/src/main/java/com/github/gimmi/spikeextjs4/Service.java
@@ -1,8 +1,19 @@
 package com.github.gimmi.spikeextjs4;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Service extends ExtDirectServlet {
+	private final Logger logger = LoggerFactory.getLogger(Service.class);
+
 	@ExtDirect
 	public String echo(String value) {
-		return value.toUpperCase();
+		logger.info("echo({})", value);
+		return value;
+	}
+
+	@ExtDirect
+	public void primitiveParameters(String stringValue, Integer intValue, Boolean boolValue, Double doubleValue) {
+		logger.info("primitiveParameters({}, {}, {}, {})", new Object[]{stringValue, intValue, boolValue, doubleValue});
 	}
 }
