471524306653c7a029b90943ce9265ca31d940f3
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2011-05-11 23:29:00 +0200

    jsbuild

---
+++ javascript/jsbuild/src/test/tests.js
@@ -0,0 +1,14 @@
+describe("Example", function() {
+
+  beforeEach(function() {
+  });
+
+  it("should succeed", function() {
+    expect(true).toBeTruthy();
+  });
+
+  it("should fail", function() {
+    expect(true).toBeFalsy();
+  });
+
+});
