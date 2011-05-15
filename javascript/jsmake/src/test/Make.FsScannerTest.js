8c120fadf3cb9d9c361c4156476b5463db831b7b
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2011-05-15 20:06:55 +0200

    jsmake

--- javascript/jsmake/src/test/Make.FsScannerTest.js
+++ javascript/jsmake/src/test/Make.FsScannerTest.js
@@ -1,23 +1,83 @@
 describe("Make.FsScanner", function() {
-	it("should traverse directory tree from base path", function() {
+	var files = {
+		'base/.': [ 
+			'readme.txt', 
+			'build.sh'
+		],
+		'base/./src': [ ],
+		'base/./src/main': [
+			'Main.js',
+			'Core.js',
+			'Helpers.js'
+		],
+		'base/./src/test': [
+			'MainTests.js',
+			'CoreTests.js',
+			'HelpersTests.js'
+		],
+	};
+	var directories = {
+		'base/.': [ 
+			'src'
+		],
+		'base/./src': [
+			'main',
+			'test'
+		],
+	};
+	
+	beforeEach(function() {
 		spyOn(Make.Sys, 'getFiles').andCallFake(function (path) {
-			return {
-				'base/.': [ 'file1', 'file2' ],
-				'base/./fld1': [ 'file3' ]
-			}[path];
+			return files[path];
 		});
 		spyOn(Make.Sys, 'getDirectories').andCallFake(function (path) {
-			return {
-				'base/.': [ 'fld1' ],
-				'base/./fld1': [],
-			}[path];
+			return directories[path];
 		});
 		spyOn(Make.Sys, 'combinePath').andCallFake(function (path1, path2) {
 			return [path1, path2].join('/');
 		});
-		
+	});
+
+	it("should traverse directory tree from base path", function() {
 		var actual = new Make.FsScanner('base').include('**/*').scan();
 
-		expect(actual).toEqual([ './file1', './file2', './fld1/file3' ]);
+		expect(actual).toEqual([ 
+			'./readme.txt', 
+			'./build.sh', 
+			'./src/main/Main.js',
+			'./src/main/Core.js',
+			'./src/main/Helpers.js',
+			'./src/test/MainTests.js',
+			'./src/test/CoreTests.js',
+			'./src/test/HelpersTests.js'
+		]);
+	});
+	
+	it('should apply include filters', function () {
+		var actual = new Make.FsScanner('base').include('**/*.js').scan();
+
+		expect(actual).toEqual([ 
+			'./src/main/Main.js',
+			'./src/main/Core.js',
+			'./src/main/Helpers.js'
+			'./src/test/MainTests.js',
+			'./src/test/CoreTests.js',
+			'./src/test/HelpersTests.js'
+		]);
+	});
+	
+	it('should apply exclude filters', function () {
+		var actual = new Make.FsScanner('base').include('**/*').exclude('**/*.js').scan();
+
+		expect(actual).toEqual([ 
+			'./readme.txt', 
+			'./build.sh'
+		]);
+	});
+	
+	it('should evaluate inclusion', function () {
+		expect(new Make.FsScanner('base')._evaluateInclusion('anything')).toBeFalsy();
+		expect(new Make.FsScanner('base').include('**/*')._evaluateInclusion('anything')).toBeTruthy();
+		expect(new Make.FsScanner('base').include('**/*').exclude('**/*_old.*')._evaluateInclusion('folder/file_old.txt')).toBeFalsy();
 	});
 });
