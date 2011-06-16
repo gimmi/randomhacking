14ea2a57642955be423ba769e15b379b049f7f96
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2011-06-16 16:40:49 +0200

    SpikeExt4

--- dotnet/SpikeExt4/app/controller/Users.js
+++ dotnet/SpikeExt4/app/controller/Users.js
@@ -14,6 +14,9 @@
 		this.control({
 			'userlist': {
 				itemdblclick: this.editUser
+			},
+			'useredit button[action=save]': {
+				click: this.updateUser
 			}
 		});
 	},
@@ -21,5 +24,15 @@
 	editUser: function (grid, record) {
 		var view = Ext.widget('useredit');
 		view.down('form').loadRecord(record);
+	},
+
+	updateUser: function (button) {
+		var win = button.up('window'),
+		    form = win.down('form'),
+		    record = form.getRecord(),
+		    values = form.getValues();
+
+		record.set(values);
+		win.close();
 	}
 });
