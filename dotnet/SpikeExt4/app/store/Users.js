27503369000620d8cd5e7db29401d98927d2276b
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2011-06-16 16:51:36 +0200

    SpikeExt4

--- dotnet/SpikeExt4/app/store/Users.js
+++ dotnet/SpikeExt4/app/store/Users.js
@@ -1,8 +1,14 @@
 ﻿Ext.define('Spike.store.Users', {
 	extend: 'Ext.data.Store',
 	model: 'Spike.model.User',
-	data: [
-        { name: 'Ed', email: 'ed@sencha.com' },
-        { name: 'Tommy', email: 'tommy@sencha.com' }
-    ]
+	autoLoad: true,
+	proxy: {
+		type: 'ajax',
+		url: 'users.json',
+		reader: {
+			type: 'json',
+			root: 'users',
+			successProperty: 'success'
+		}
+	}
 });
