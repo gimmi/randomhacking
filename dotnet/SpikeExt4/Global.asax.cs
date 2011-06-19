841796d2936ef968f43e82d6a94a2a209451bb2f
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2011-06-19 21:12:51 +0200

    SpikeExt4

--- dotnet/SpikeExt4/Global.asax.cs
+++ dotnet/SpikeExt4/Global.asax.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Web;
+using Castle.MicroKernel.Registration;
+using Castle.Windsor;
 using ExtDirectHandler;
 using ExtDirectHandler.Configuration;
 
@@ -7,12 +9,21 @@ namespace SpikeExt4
 {
 	public class Global : HttpApplication
 	{
+		private IWindsorContainer _container;
+
 		protected void Application_Start(object sender, EventArgs e)
 		{
+			_container = new WindsorContainer();
+			_container.Register(
+				Component.For<TicketRepository>().LifeStyle.Transient,
+				Component.For<FilterClauseRepository>().LifeStyle.Transient
+				);
+
 			DirectHttpHandler.SetMetadata(new ReflectionConfigurator()
 			                              	.RegisterType<TicketRepository>()
 			                              	.RegisterType<FilterClauseRepository>()
 			                              	.BuildMetadata());
+			DirectHttpHandler.SetObjectFactory(new WindsorObjectFactory(_container));
 		}
 
 		protected void Session_Start(object sender, EventArgs e) {}
@@ -26,5 +37,29 @@ namespace SpikeExt4
 		protected void Session_End(object sender, EventArgs e) {}
 
 		protected void Application_End(object sender, EventArgs e) {}
+
+		#region Nested type: WindsorObjectFactory
+
+		private class WindsorObjectFactory : ObjectFactory
+		{
+			private readonly IWindsorContainer _container;
+
+			public WindsorObjectFactory(IWindsorContainer container)
+			{
+				_container = container;
+			}
+
+			public override object GetInstance(Type type)
+			{
+				return _container.Resolve(type);
+			}
+
+			public override void Release(object instance)
+			{
+				_container.Release(instance);
+			}
+		}
+
+		#endregion
 	}
 }
