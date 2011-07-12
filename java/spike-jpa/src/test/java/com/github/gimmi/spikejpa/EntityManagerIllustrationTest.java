af465560de29bfa30ff5f5a12e73bc126600872f
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2011-07-12 20:48:53 +0200

    spike-jpa

--- java/spike-jpa/src/test/java/com/github/gimmi/spikejpa/EntityManagerIllustrationTest.java
+++ java/spike-jpa/src/test/java/com/github/gimmi/spikejpa/EntityManagerIllustrationTest.java
@@ -8,9 +8,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertNull;
@@ -22,10 +20,7 @@ public class EntityManagerIllustrationTest {
 
 	@BeforeClass
 	public static void beforeClass() {
-		Map<String, String> props = new HashMap<String, String>();
-		props.put("javax.persistence.jdbc.url", "jdbc:derby:memory:test_derby_db;create=true");
-		props.put("hibernate.hbm2ddl.auto", "create");
-		entityManagerFactory = Persistence.createEntityManagerFactory("spikejpa", props);
+		entityManagerFactory = Persistence.createEntityManagerFactory("com.github.gimmi.spikejpa");
 	}
 
 	@AfterClass
@@ -63,15 +58,6 @@ public class EntityManagerIllustrationTest {
 		entityManager.getTransaction().begin();
 		entityManager.persist(event);
 		assertThat(event.getVersion(), equalTo(0));
-		entityManager.getTransaction().commit();
-		entityManager.close();
-
-		entityManager = entityManagerFactory.createEntityManager();
-		entityManager.getTransaction().begin();
-		List<Event> result = entityManager.createQuery("from Event", Event.class).getResultList();
-		assertEquals(1, result.size());
-		assertEquals("Our very first event!", result.get(0).getTitle());
-		assertEquals(event.getId(), result.get(0).getId());
 		entityManager.getTransaction().commit();
 		entityManager.close();
 	}
