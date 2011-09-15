2b37ef030d47af7e739b0b0af440f02111657d98
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2011-09-15 22:35:23 +0200

    SpikeHttpAuth

---
+++ dotnet/SpikeHttpAuth/Handler.ashx.cs
@@ -0,0 +1,18 @@
+﻿using System.Web;
+
+namespace SpikeHttpAuth
+{
+	public class Handler : IHttpHandler
+	{
+		public bool IsReusable
+		{
+			get { return false; }
+		}
+
+		public void ProcessRequest(HttpContext context)
+		{
+			context.Response.ContentType = "text/plain";
+			context.Response.Write(context.User.Identity.Name);
+		}
+	}
+}
