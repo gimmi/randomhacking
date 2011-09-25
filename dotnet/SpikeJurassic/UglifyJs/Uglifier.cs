c03c6ec350db8a0405f7bf499a0fadbe29da3595
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2011-09-25 21:58:28 +0200

    SpikeJurassic

---
+++ dotnet/SpikeJurassic/UglifyJs/Uglifier.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using System.Reflection;
+using System.Text;
+using Jurassic;
+
+namespace SpikeJurassic.UglifyJs
+{
+	public class Uglifier
+	{
+		public string Uglify(string script)
+		{
+			string uglifyCode = new StringBuilder()
+				.AppendLine("var exports = {}; function require() { return exports; };")
+				.AppendLine(ReadResource("parse-js.js"))
+				.AppendLine(ReadResource("process.js"))
+				.AppendLine("value = gen_code(ast_squeeze(ast_mangle(parse(value))));")
+				.ToString();
+			var engine = new ScriptEngine();
+			engine.SetGlobalValue("value", script);
+			engine.Execute(uglifyCode);
+			return engine.GetGlobalValue<string>("value");
+		}
+
+		private string ReadResource(string fileName)
+		{
+			using(var s = new StreamReader(Assembly.GetExecutingAssembly().GetManifestResourceStream(GetType(), fileName)))
+			{
+				return s.ReadToEnd();
+			}
+		}
+	}
+}
---
+++ dotnet/SpikeJurassic/UglifyJs/UglifierTest.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace SpikeJurassic.UglifyJs
+{
+	[TestFixture]
+	public class UglifierTest
+	{
+		[Test]
+		public void Tt()
+		{
+			Assert.That(new Uglifier().Uglify("(function () { var ciao = 1; })();"), Is.EqualTo("(function(){var a=1})()"));
+		}
+	}
+}
