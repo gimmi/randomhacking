3479b26684195eec262c2c7166aa81d1f934ece4
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2011-10-21 23:21:28 +0200

    SpikeEmitMapper

---
+++ dotnet/SpikeEmitMapper/Class1.cs
@@ -0,0 +1,35 @@
+﻿using EmitMapper;
+using NUnit.Framework;
+
+namespace SpikeEmitMapper
+{
+	[TestFixture]
+	public class Class1
+	{
+		private ObjectMapperManager _target;
+
+		public class ClassA
+		{
+			public string StringValue;
+			public int IntValue;
+		}
+
+		public class ClassB
+		{
+			public string StringValue;
+			public int IntValue;
+		}
+
+		[Test]
+		public void Tt()
+		{
+			_target = new ObjectMapperManager();
+
+			var classA = new ClassA {StringValue = "a string", IntValue = 123};
+			ClassB classB = _target.GetMapper<ClassA, ClassB>().Map(classA);
+
+			Assert.That(classB.StringValue, Is.EqualTo(classA.StringValue));
+			Assert.That(classB.IntValue, Is.EqualTo(classA.IntValue));
+		}
+	}
+}
