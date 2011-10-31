69e51e4c38698ad89f05ff0560cdde6464a75f81
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2011-10-31 08:54:36 +0100

    SpikeSeleniumWebdriver

---
+++ dotnet/SpikeSeleniumWebdriver/Class1.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace SpikeSeleniumWebdriver
+{
+	[TestFixture]
+	public class Class1
+	{
+		private IWebDriver driver;
+
+		[SetUp]
+		public void SetUp()
+		{
+			var profile = new FirefoxProfile("FirefoxProfile", true);
+			driver = new FirefoxDriver(profile);
+		}
+
+		[TearDown]
+		public void TearDown()
+		{
+			driver.Quit();
+		}
+
+		[Test]
+		public void Tt()
+		{
+			driver.Navigate().GoToUrl("http://www.google.com");
+			IWebElement query = driver.FindElement(By.Name("q"));
+			query.SendKeys("Cheese");
+			Assert.That(driver.Title, Is.EqualTo("Google"));
+		}
+	}
+}
