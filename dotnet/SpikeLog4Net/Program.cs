42b9165811888a42508153b7c104942350239816
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2011-11-06 20:27:58 +0100

    SpikeLog4Net

---
+++ dotnet/SpikeLog4Net/Program.cs
@@ -0,0 +1,21 @@
+﻿using log4net;
+using log4net.Config;
+
+namespace SpikeLog4Net
+{
+	public class Program
+	{
+		private static readonly ILog Log = LogManager.GetLogger(typeof(Program));
+
+		private static void Main()
+		{
+			XmlConfigurator.Configure();
+
+			Log.Debug("debug");
+			Log.Info("info");
+			Log.Warn("warn");
+			Log.Error("error");
+			Log.Fatal("fatal");
+		}
+	}
+}
