931c1cee000f3e2431e416161e776afeeeb92503
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2011-11-07 16:00:25 +0100

    SpikeLog4Net

--- dotnet/SpikeLog4Net/Program.cs
+++ dotnet/SpikeLog4Net/Program.cs
@@ -1,4 +1,6 @@
-﻿using log4net;
+﻿using System.Data;
+using System.Data.SqlClient;
+using log4net;
 using log4net.Config;
 
 namespace SpikeLog4Net
@@ -16,6 +18,61 @@ namespace SpikeLog4Net
 			Log.Warn("warn");
 			Log.Error("error");
 			Log.Fatal("fatal");
+		}
+
+		private static void AdoNetAppenderProgrammaticConfiguration()
+		{
+			var adoNetAppender = new AdoNetAppender
+			{
+				ConnectionType = typeof(SqlConnection).AssemblyQualifiedName,
+				ConnectionString = ConfigurationManager.ConnectionStrings["CritechRssStorage"].ConnectionString,
+				BufferSize = 1,
+				CommandText = "INSERT INTO Logs(Date, Thread, Level, Logger, Message, Exception) VALUES(@Date, @Thread, @Level, @Logger, @Message, @Exception)"
+			};
+			adoNetAppender.AddParameter(new AdoNetAppenderParameter
+			{
+				ParameterName = "@Date",
+				DbType = DbType.DateTime,
+				Layout = new RawTimeStampLayout()
+			});
+			adoNetAppender.AddParameter(new AdoNetAppenderParameter
+			{
+				ParameterName = "@Thread",
+				DbType = DbType.String,
+				Size = 255,
+				Layout = new Layout2RawLayoutAdapter(new PatternLayout("%thread"))
+			});
+			adoNetAppender.AddParameter(new AdoNetAppenderParameter
+			{
+				ParameterName = "@Level",
+				DbType = DbType.String,
+				Size = 50,
+				Layout = new Layout2RawLayoutAdapter(new PatternLayout("%level"))
+			});
+			adoNetAppender.AddParameter(new AdoNetAppenderParameter
+			{
+				ParameterName = "@Logger",
+				DbType = DbType.String,
+				Size = 255,
+				Layout = new Layout2RawLayoutAdapter(new PatternLayout("%logger"))
+			});
+			adoNetAppender.AddParameter(new AdoNetAppenderParameter
+			{
+				ParameterName = "@Message",
+				DbType = DbType.String,
+				Size = 4000,
+				Layout = new Layout2RawLayoutAdapter(new PatternLayout("%message"))
+			});
+			adoNetAppender.AddParameter(new AdoNetAppenderParameter
+			{
+				ParameterName = "@Exception",
+				DbType = DbType.String,
+				Size = 4000,
+				Layout = new Layout2RawLayoutAdapter(new ExceptionLayout())
+			});
+			adoNetAppender.ActivateOptions();
+			var hierarchy = (Hierarchy)LogManager.GetRepository();
+			hierarchy.Root.AddAppender(adoNetAppender);
 		}
 	}
 }
