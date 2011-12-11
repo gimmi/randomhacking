237a3995a09963f123244a79bc605a78c5278d1e
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2011-12-11 17:09:20 +0100

    spike-node-connect

--- javascript/spike-node-connect/app.js
+++ javascript/spike-node-connect/app.js
@@ -1,10 +1,25 @@
 var connect = require('connect'),
-	rpc = require('./rpc');
+	rpc = require('./rpc'),
+	pg = require('pg'),
+	dbCfg = {
+		user: 'postgres',
+		database: 'postgres',
+		password: '',
+		port: 5432,
+		host: 'localhost'
+	};
 
 connect.createServer()
 	.use(connect.favicon())
 	.use(connect.logger())
 	.use(connect.directory(__dirname))
 	.use(connect.static(__dirname))
-	.use(rpc.rpc())
+	.use(function (req, res, next) {
+		pg.connect(dbCfg, function(err, client) {
+			client.query("SELECT version();", function(err, result) {
+				res.writeHead(200, {'Content-Type': 'text/plain'});
+				res.end(JSON.stringify(result.rows));
+			});
+		});
+	})
 	.listen(8080);
