1e655da351a3daa18d64a6ee6f834abb319a55a8
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2012-01-11 17:59:22 +0100

    SpikeHttpAuth

--- dotnet/SpikeHttpAuth/HttpBasicAuthHttpModule.cs
+++ dotnet/SpikeHttpAuth/HttpBasicAuthHttpModule.cs
@@ -40,5 +40,44 @@ namespace SpikeHttpAuth
 			}
 			return false; // app.Context.User = new GenericPrincipal(new GenericIdentity("", type), new string[0]);
 		}
+
+		/* This snippet shows authentication using MembershipProvider of ASP.NET.
+
+in web.config:
+    <system.web>
+        <compilation debug="true" targetFramework="4.0" />
+		<machineKey validationKey="xxxxx" decryptionKey="xxxxx" decryption="3DES" validation="SHA1" />
+		<membership defaultProvider="SqlMembershipProvider">
+			<providers>
+				<clear />
+				<add name="SqlMembershipProvider" 
+					 type="System.Web.Security.SqlMembershipProvider" 
+					 connectionStringName="SqlMembershipProvider"
+					 enablePasswordRetrieval="true" 
+					 enablePasswordReset="true" 
+					 requiresQuestionAndAnswer="false" 
+					 minRequiredPasswordLength="7" 
+					 minRequiredNonalphanumericCharacters="0" 
+					 requiresUniqueEmail="false" 
+					 passwordFormat="Encrypted" 
+					 applicationName="DotNetNuke" 
+					 description="Stores and retrieves membership data from the local Microsoft SQL Server database" />
+			</providers>
+		</membership>
+	</system.web>
+
+//			MembershipUser membershipUser = Membership.GetUser("9182");
+//			bool unlockUser = membershipUser.UnlockUser();
+//			string pwd = membershipUser.GetPassword();
+			HttpBasicAuth.AuthenticateRequest("Insert just the user name", delegate(string username, string password) {
+				if(!Membership.ValidateUser(username, password))
+				{
+					return false;
+				}
+				HttpContext.Current.User = new GenericPrincipal(new GenericIdentity(Membership.GetUser(username).UserName, "HTTP Basic Auth"), null);
+				return true;
+			});
+		 
+		 */
 	}
 }
