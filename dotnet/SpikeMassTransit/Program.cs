58e5533d73101688fb977d1b453d06a9837177ff
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2012-03-23 21:19:27 +0100

    SpikeMassTransit

---
+++ dotnet/SpikeMassTransit/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using MassTransit;
+
+namespace SpikeMassTransit
+{
+	internal class Program
+	{
+		private static void Main()
+		{
+			Console.WriteLine("starting...");
+
+			Bus.Initialize(sbc => {
+				Console.WriteLine("configuring...");
+				sbc.ReceiveFrom("loopback://localhost/mt_test");
+				sbc.Subscribe(subs => {
+					subs.Handler<YourMessage>(msg => Console.WriteLine(msg.Text));
+				});
+				sbc.BeforeConsumingMessage(() => {
+					Console.WriteLine("before");
+				});
+				sbc.AfterConsumingMessage(() => {
+					Console.WriteLine("after");
+				});
+			});
+			Console.WriteLine("publishing...");
+			Bus.Instance.Publish(new YourMessage { Text = "Hi" });
+			Console.WriteLine("waiting... press a key to exit.");
+			Console.ReadKey();
+		}
+	}
+}
---
+++ dotnet/SpikeMassTransit/YourMessage.cs
@@ -0,0 +1,7 @@
+namespace SpikeMassTransit
+{
+	public class YourMessage
+	{
+		public string Text { get; set; }
+	}
+}
