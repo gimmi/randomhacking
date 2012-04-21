f57bf5541604760c886998dfc22f264f6540ca16
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2012-04-21 17:16:40 +0200

    spike-express

--- javascript/spike-express/web.js
+++ javascript/spike-express/web.js
@@ -1,12 +1,16 @@
-var express = require('express');
+var express = require('express'),
+	app = express.createServer();
 
-var app = express.createServer(express.logger());
+app.configure(function () {
+	app.use(express.logger());
+	app.use(express.static(__dirname + '/public'));
+});
 
-app.get('/', function(request, response) {
-	response.send('Hello World!');
+app.get('/', function(req, res){
+  res.send('hello world');
 });
 
 var port = process.env.PORT || 3000;
-app.listen(port, function() {
+app.listen(3000, function() {
 	console.log("Listening on " + port);
 });
