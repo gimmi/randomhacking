839989de925287db65a87a6c5a07db422f2273f2
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2012-07-10 22:17:47 +0200

    SpikeLog4Net

--- dotnet/SpikeLog4Net/Program.cs
+++ dotnet/SpikeLog4Net/Program.cs
@@ -1,7 +1,13 @@
-﻿using System.Data;
+﻿using System.Configuration;
+using System.Data;
 using System.Data.SqlClient;
+using System.IO;
+using System.Text;
 using log4net;
+using log4net.Appender;
 using log4net.Config;
+using log4net.Layout;
+using log4net.Repository.Hierarchy;
 
 namespace SpikeLog4Net
 {
@@ -11,7 +17,69 @@ namespace SpikeLog4Net
 
 		private static void Main()
 		{
+			log4net.Util.LogLog.InternalDebugging = true;
+			DatabaseLog();
 			XmlConfigurator.Configure();
+
+			Log.Debug("debug");
+			Log.Info("info");
+			Log.Warn("warn");
+			Log.Error("error");
+			Log.Fatal("fatal");
+		}
+
+		private static void DatabaseLog()
+		{
+			XmlConfigurator.Configure(new MemoryStream(Encoding.UTF8.GetBytes(@"
+<log4net>
+	<appender name='AdoNetAppender' type='log4net.Appender.AdoNetAppender'>
+		<bufferSize value='1' />
+		<connectionType value='System.Data.SqlClient.SqlConnection' />
+		<connectionString value='data source=.\SQLEXPRESS;initial catalog=ADOUtilstests;integrated security=true' />
+		<commandText value='INSERT INTO [Logs](App, Thread, Level, Logger, Message, Exception) VALUES(@App, @Thread, @Level, @Logger, @Message, @Exception)' />
+		<parameter>
+			<parameterName value='@App' />
+			<dbType value='String' />
+			<size value='255' />
+			<layout type='log4net.Layout.PatternLayout' value='%appdomain' />
+		</parameter>
+		<parameter>
+			<parameterName value='@Thread' />
+			<dbType value='String' />
+			<size value='255' />
+			<layout type='log4net.Layout.PatternLayout' value='%thread' />
+		</parameter>
+		<parameter>
+			<parameterName value='@Level' />
+			<dbType value='String' />
+			<size value='50' />
+			<layout type='log4net.Layout.PatternLayout' value='%level' />
+		</parameter>
+		<parameter>
+			<parameterName value='@Logger' />
+			<dbType value='String' />
+			<size value='255' />
+			<layout type='log4net.Layout.PatternLayout' value='%logger' />
+		</parameter>
+		<parameter>
+			<parameterName value='@Message' />
+			<dbType value='String' />
+			<size value='-1' />
+			<layout type='log4net.Layout.PatternLayout' value='%message' />
+		</parameter>
+		<parameter>
+			<parameterName value='@Exception' />
+			<dbType value='String' />
+			<size value='-1' />
+			<layout type='log4net.Layout.ExceptionLayout' />
+		</parameter>
+	</appender>
+	<root>
+		<level value='ALL' />
+		<appender-ref ref='AdoNetAppender' />
+	</root>
+</log4net>
+")));
 
 			Log.Debug("debug");
 			Log.Info("info");
