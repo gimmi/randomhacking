79dae77a9ba089f86acead907f39cfd293c1758b
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2012-09-01 15:30:30 +0200

    devwiki

--- python/devwiki/devwiki/WikiWsgiApp.py
+++ python/devwiki/devwiki/WikiWsgiApp.py
@@ -1,7 +1,6 @@
 from markdown import markdown
 import os, codecs
 
-# TODO use http://lukearno.com/projects/static/
 class HttpStatusWsgiApp:
     def __init__(self, status):
         self.status = status
@@ -11,8 +10,18 @@ class HttpStatusWsgiApp:
         return [self.status]
 
 
+class FaviconWsgiApp:
+    def __init__(self, wsgiapp):
+        self.wsgiapp = wsgiapp
+
+    def __call__(self, environ, start_response):
+        if environ['REQUEST_METHOD'] == 'GET' and environ.get('PATH_INFO', '/') == '/favicon.ico':
+            return HttpStatusWsgiApp('404 Not Found')(environ, start_response)
+        return self.wsgiapp(environ, start_response)
+
+
 class WikiWsgiApp:
-    def __init__(self, rootdir=os.getcwd(), encoding='utf-8', wikiext='.wiki', pagetemplate='<html><body>{0}</body></html>', wsgiapp=HttpStatusWsgiApp('404 Not Found')):
+    def __init__(self, rootdir=os.getcwd(), encoding='utf-8', wikiext='.wiki', pagetemplate='<!DOCTYPE html><html><head></head><body>{0}</body></html>', wsgiapp=HttpStatusWsgiApp('404 Not Found')):
         self.rootdir = rootdir
         self.encoding = encoding
         self.wikiext = wikiext
@@ -21,8 +30,7 @@ class WikiWsgiApp:
 
     def __call__(self, environ, start_response):
         if environ['REQUEST_METHOD'] != 'GET':
-            start_response('404 Not Found', [('Content-Type', 'text/plain')])
-            return ['Not Found']
+            return HttpStatusWsgiApp('405 Method Not Allowed')(environ, start_response)
         file = environ.get('PATH_INFO', '/')
         file = os.path.join(self.rootdir, file.lstrip('/'))
         if os.path.isfile(file) and file.endswith(self.wikiext):
--- python/devwiki/devwiki/__main__.py
+++ python/devwiki/devwiki/__main__.py
@@ -1,10 +1,18 @@
 import devwiki.WikiWsgiApp
 
 from wsgiref.simple_server import make_server
-import static
+import static, codecs, pkg_resources
 
 rootdir = 'c:\\users\\gimmi\\temp\\wiki'
-make_server('localhost', 8000, devwiki.WikiWsgiApp.WikiWsgiApp(rootdir, wsgiapp=static.Cling(rootdir))).serve_forever()
+encoding='utf-8'
+
+pagetemplate = pkg_resources.resource_string(__name__, 'resources/template.html').decode(encoding)
+
+app = static.Cling(rootdir)
+app = devwiki.WikiWsgiApp.WikiWsgiApp(rootdir, wsgiapp=app, encoding=encoding, pagetemplate=pagetemplate)
+app = devwiki.WikiWsgiApp.FaviconWsgiApp(app)
+
+make_server('localhost', 8000, app).serve_forever()
 
 
 
