87b35a1e352cf52fbec0d8555037427d9033b90c
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2012-09-22 09:47:53 +0200

    logtopng

--- python/logtopng/parse.py
+++ python/logtopng/parse.py
@@ -3,6 +3,10 @@ import re
 
 pattern = re.compile(r': (\d+)? => (\d+)$')
 transitions = {}
+states = {
+	None: 'New',
+	100: 'Error'
+}
 
 for file_name in glob('*.txt'):
 	with open(file_name, 'r') as f:
@@ -10,11 +14,15 @@ for file_name in glob('*.txt'):
 			match = pattern.search(line)
 			if not match: continue
 			transition = match.groups()
+			if transition[0] not in states:
+				states[transition[0]] = transition[0] + ': unknown'
 			transitions[transition] = transitions[transition]+1 if transition in transitions else 1
 
 
 with open('out.dot', 'w') as f:
 	f.write('digraph G {')
+	for state, descr in states.iteritems():
+		f.write('\t{0} [label="{0} - {1}"];'.format(state, descr))
 	for transition, times in transitions.iteritems():
 		f.write('\t{0} -> {1} [label="{2}"];'.format(transition[0], transition[1], times))
 	f.write('}')
