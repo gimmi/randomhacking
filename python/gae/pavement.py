79f0ab3a24f0a64ae606e5d94ee9092a951e572a
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2013-02-24 08:12:15 +0100

    python gae

--- python/gae/pavement.py
+++ python/gae/pavement.py
@@ -26,3 +26,18 @@ def create_env():
 			gae_lib_path = os.path.join(options.gae_sdk_path, 'lib', gae_lib)
 			gae_pth_file.write(gae_lib_path + '\n')
 
+# See http://stackoverflow.com/questions/6260149
+def symlink(source, link_name):
+	import os
+	os_symlink = getattr(os, "symlink", None)
+	if callable(os_symlink):
+		os_symlink(source, link_name)
+	else:
+		import ctypes
+		csl = ctypes.windll.kernel32.CreateSymbolicLinkW
+		csl.argtypes = (ctypes.c_wchar_p, ctypes.c_wchar_p, ctypes.c_uint32)
+		csl.restype = ctypes.c_ubyte
+		flags = 1 if os.path.isdir(source) else 0
+		if csl(link_name, source, flags) == 0:
+			raise ctypes.WinError()
+
