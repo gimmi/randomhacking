7991656b7762ed36afdf0b96b79ac441f8b28c40
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2013-03-22 22:15:32 +0100

    spike-scala-scalatra-mvn

---
+++ java/spike-scala-scalatra-mvn/src/test/scala/com/github/gimmi/spikescalaservlet/MockSpec.scala
@@ -0,0 +1,14 @@
+package com.github.gimmi.spikescalaservlet
+
+import org.specs2.mock.Mockito
+import org.specs2.mutable._
+
+class MockSpec extends Specification with Mockito {
+	"Mocking an object" should {
+		"not call methods" in {
+			val m = mock[java.util.List[String]]
+			m.get(0) returns "one"
+			m.get(0) must be equalTo("one")
+		}
+	}
+}
