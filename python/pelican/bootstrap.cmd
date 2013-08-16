3ae7e7f3a1944440c20c29de2235097b7cb627af
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2013-08-16 10:43:48 +0200

    pelican

---
+++ python/pelican/bootstrap.cmd
@@ -0,0 +1,6 @@
+cd %~dp0
+python -mvenv .env
+curl -o .env\distribute_setup.py http://python-distribute.org/distribute_setup.py
+.env\Scripts\python .env\distribute_setup.py
+.env\Scripts\easy_install pip
+.env\Scripts\pip install -r requirements.txt
