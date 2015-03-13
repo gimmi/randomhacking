1b403e01d7dc4b9e7278322e313822429b91eda6
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2015-03-13 15:00:21 +0100

    Update MyApplication.java

--- java/jarweb/src/main/java/com/github/gimmi/jarweb/MyApplication.java
+++ java/jarweb/src/main/java/com/github/gimmi/jarweb/MyApplication.java
@@ -1,16 +1,18 @@
 package com.github.gimmi.jarweb;
 
 import org.glassfish.hk2.utilities.binding.AbstractBinder;
+import org.glassfish.jersey.process.internal.RequestScoped;
 import org.glassfish.jersey.server.ResourceConfig;
 
 public class MyApplication extends ResourceConfig {
 
     public MyApplication() {
-        packages("com.github.gimmi.jarweb");
+        register(MyResource.class);
+        
         register(new AbstractBinder() {
             @Override
             protected void configure() {
-                bind(MyObject.class).to(MyObject.class);
+                bind(MyObject.class).to(MyObject.class).in(RequestScoped.class);
             }
         });
     }
