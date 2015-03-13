9e50294f73000a71016a6a6e1f29f4b6b3979b04
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2015-03-13 16:00:59 +0100

    Update MyResource.java

--- java/jarweb/src/main/java/com/github/gimmi/jarweb/MyResource.java
+++ java/jarweb/src/main/java/com/github/gimmi/jarweb/MyResource.java
@@ -1,20 +1,29 @@
 package com.github.gimmi.jarweb;
 
 import javax.inject.Inject;
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 @Path("myresource")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class MyResource {
-    
+
     @Inject
     MyObject myObject;
 
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String getIt() {
-        return myObject.getMessage();
+    public JsonObject getIt() {
+        return Json.createObjectBuilder()
+            .add("p1", myObject.getMessage())
+            .add("p2", 42)
+            .add("p3", true)
+            .addNull("p4")
+            .build();
     }
 }
