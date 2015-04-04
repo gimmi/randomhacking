33be8eb8747d795f07455e4c9c91709d8523f98a
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2015-04-04 16:32:38 +0200

    JsonParser

--- dotnet/JsonParser/JsonParserTest.cs
+++ dotnet/JsonParser/JsonParserTest.cs
@@ -45,10 +45,26 @@ namespace JsonParser
                 'trueProp': true,
                 'falseProp': false,
                 'nullProp': null,
-                'aryProp': ['val', -3.14e2, true, false, null],
-                'objProp': {'a': 1},
                 'emptyAry': [],
-                'emptyObj': {}    
+                'emptyObj': {},
+                'aryProp': [
+					'val', 
+					-3.14e2, 
+					true, 
+					false, 
+					null, 
+					[], 
+					{}
+				],
+                'objProp': {
+					'strProp': 'val',
+					'numProp': -3.14e2,
+					'trueProp': true,
+					'falseProp': false,
+					'nullProp': null,
+					'emptyAry': [],
+					'emptyObj': {}
+				}
             }".Replace('\'', '"');
 			dynamic o = JsonParser.Parse(json);
 
@@ -58,16 +74,24 @@ namespace JsonParser
 			Assert.That(o["trueProp"], Is.True);
 			Assert.That(o["falseProp"], Is.False);
 			Assert.That(o["nullProp"], Is.Null);
-			Assert.That(o["aryProp"], Has.Count.EqualTo(5));
+			Assert.That(o["emptyAry"], Has.Count.EqualTo(0));
+			Assert.That(o["emptyObj"], Has.Count.EqualTo(0));
+			Assert.That(o["aryProp"], Has.Count.EqualTo(7));
 			Assert.That(o["aryProp"][0], Is.EqualTo("val"));
 			Assert.That(o["aryProp"][1], Is.EqualTo(-314));
 			Assert.That(o["aryProp"][2], Is.True);
 			Assert.That(o["aryProp"][3], Is.False);
 			Assert.That(o["aryProp"][4], Is.Null);
-			Assert.That(o["objProp"], Has.Count.EqualTo(1));
-			Assert.That(o["objProp"]["a"], Is.EqualTo(1));
-			Assert.That(o["emptyAry"], Has.Count.EqualTo(0));
-			Assert.That(o["emptyObj"], Has.Count.EqualTo(0));
+			Assert.That(o["aryProp"][5], Has.Count.EqualTo(0));
+			Assert.That(o["aryProp"][6], Has.Count.EqualTo(0));
+			Assert.That(o["objProp"], Has.Count.EqualTo(7));
+			Assert.That(o["objProp"]["strProp"], Is.EqualTo("val"));
+			Assert.That(o["objProp"]["numProp"], Is.EqualTo(-314));
+			Assert.That(o["objProp"]["trueProp"], Is.True);
+			Assert.That(o["objProp"]["falseProp"], Is.False);
+			Assert.That(o["objProp"]["nullProp"], Is.Null);
+			Assert.That(o["objProp"]["emptyAry"], Has.Count.EqualTo(0));
+			Assert.That(o["objProp"]["emptyObj"], Has.Count.EqualTo(0));
 		}
 	}
 }
