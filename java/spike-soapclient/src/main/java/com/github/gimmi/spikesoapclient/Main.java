53a47a0b9268ce39bc77acdb887a9f3a156af53a
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2015-11-23 10:01:58 +0100

    spike-soapclient

--- java/spike-soapclient/src/main/java/com/github/gimmi/spikesoapclient/Main.java
+++ java/spike-soapclient/src/main/java/com/github/gimmi/spikesoapclient/Main.java
@@ -4,54 +4,44 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.xml.soap.*;
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 public class Main {
-    private static Logger logger = LoggerFactory.getLogger(Main.class);
+	private static Logger logger = LoggerFactory.getLogger(Main.class);
 
+	public static void main(String args[]) throws Exception {
+		logger.info("Application started");
+		try {
+			SOAPConnectionFactory soapConnectionFactory = SOAPConnectionFactory.newInstance();
+			SOAPConnection soapConnection = soapConnectionFactory.createConnection();
 
-    public static void main(String args[]) throws Exception {
-        // Create SOAP Connection
-        SOAPConnectionFactory soapConnectionFactory = SOAPConnectionFactory.newInstance();
-        SOAPConnection soapConnection = soapConnectionFactory.createConnection();
+			String url = "http://ws.cdyne.com/emailverify/Emailvernotestemail.asmx";
+			SOAPMessage soapMessage = MessageFactory.newInstance().createMessage();
+			soapMessage.getMimeHeaders().addHeader("SOAPAction", "http://ws.cdyne.com/VerifyEmail");
+			SOAPElement soapBodyElem = soapMessage.getSOAPPart().getEnvelope().getBody().addChildElement("VerifyEmail", "", "http://ws.cdyne.com/");
 
-        // Send SOAP Message to SOAP Server
-        String url = "http://ws.cdyne.com/emailverify/Emailvernotestemail.asmx";
-        SOAPMessage soapRequest = createSOAPRequest();
+			soapBodyElem.addChildElement("email").addTextNode("mutantninja@gmail.com");
+			soapBodyElem.addChildElement("LicenseKey").addTextNode("123");
 
-        System.out.print("Request SOAP Message:");
-        soapRequest.writeTo(System.out);
-        System.out.println();
+			soapMessage.saveChanges();
 
-        SOAPMessage soapResponse = soapConnection.call(soapRequest, url);
+			File inFile = new File("input.xml").getCanonicalFile();
+			logger.info("Request file: {}", inFile);
+			OutputStream reqOs = new FileOutputStream(inFile);
+			soapMessage.writeTo(reqOs);
 
-        // print SOAP Response
-        System.out.print("Response SOAP Message:");
-        soapResponse.writeTo(System.out);
-        System.out.println();
+			SOAPMessage soapResponse = soapConnection.call(soapMessage, url);
 
-        soapConnection.close();
-    }
+			File outFile = new File("output.xml").getCanonicalFile();
+			logger.info("Response file: {}", outFile);
+			OutputStream respOs = new FileOutputStream(outFile);
+			soapResponse.writeTo(respOs);
 
-    private static SOAPMessage createSOAPRequest() throws Exception {
-        MessageFactory messageFactory = MessageFactory.newInstance();
-        SOAPMessage soapMessage = messageFactory.createMessage();
-
-        SOAPPart soapPart = soapMessage.getSOAPPart();
-
-        String xmlns = "http://ws.cdyne.com/";
-
-        SOAPEnvelope envelope = soapPart.getEnvelope();
-
-        SOAPBody soapBody = envelope.getBody();
-        SOAPElement soapBodyElem = soapBody.addChildElement("VerifyEmail", "", xmlns);
-        soapBodyElem.addChildElement("email").addTextNode("mutantninja@gmail.com");
-        soapBodyElem.addChildElement("LicenseKey").addTextNode("123");
-
-//        MimeHeaders headers = soapMessage.getMimeHeaders();
-//        headers.addHeader("SOAPAction", xmlns + "VerifyEmail");
-
-        soapMessage.saveChanges();
-
-        return soapMessage;
-    }
+			soapConnection.close();
+		} catch (Exception e) {
+			logger.error("Exception thrown", e);
+		}
+	}
 }
