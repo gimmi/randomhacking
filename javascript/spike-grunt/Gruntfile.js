345f5246ccde33550ad62e2773cf3f553136b252
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2016-08-12 14:54:42 +0200

    spike-grunt

--- javascript/spike-grunt/Gruntfile.js
+++ javascript/spike-grunt/Gruntfile.js
@@ -19,37 +19,26 @@ module.exports = function(grunt) {
                 }]
             }
         },
+        ts: {
+            default: {
+                src: 'public/**/*.ts',
+                options: {
+                    module: 'amd'
+                }
+            }
+        },
         watch: {
             scripts: {
                 files: 'public/**/*.ts',
                 tasks: 'ts'
             }
-        },
-        ts: {
-            default: {
-                src: 'public/**/*.ts',
-                // out: 'public/app.js',
-                options: {
-                    module: 'amd'
-                }
-            }
         }
     });
 
-    // grunt connect
     grunt.loadNpmTasks('grunt-contrib-connect');
-
-    // See https://github.com/drewzboto/grunt-connect-proxy/pull/140
-    grunt.loadNpmTasks('grunt-connect-proxy');
-
+    grunt.loadNpmTasks('grunt-connect-proxy'); // See https://github.com/drewzboto/grunt-connect-proxy/pull/140
     grunt.loadNpmTasks('grunt-contrib-watch');
-
     grunt.loadNpmTasks("grunt-ts");
 
-    // grunt
     grunt.registerTask('default', ['ts', 'configureProxies:server', 'connect:server', 'watch:scripts']);
-
-    grunt.registerTask('compile', 'A fake compiler', function() {
-        grunt.log.writeln('Compiling...');
-    });
 };
