0c368b73b28a556933081f819936d7630b1c2809
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2016-08-19 12:54:05 +0200

    spike-typescript

--- javascript/spike-typescript/server/app.ts
+++ javascript/spike-typescript/server/app.ts
@@ -1,1 +1,9 @@
+const iterations: number[] = [1, 2, 3, 4, 5];
+
+console.dir(iterations);
+
+iterations.forEach(function(iteration) {
+    console.log(`Iteration ${iteration}...`);
+});
+
 process.exit(1);
