155d09324ab85f939308bee9f8442c993a6f07c7
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2016-09-01 16:04:36 +0200

    webpack-angular

--- javascript/webpack-angular/webpack.config.js
+++ javascript/webpack-angular/webpack.config.js
@@ -4,13 +4,25 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: {
-        vendor: ['angular', 'angular-route'],
+        vendor: [
+            'angular',
+            'angular-route',
+            'angular-aria',
+            'angular-animate',
+            'angular-material',
+            'angular-material/angular-material.css'
+        ],
         app: "./src/App.js"
     },
     devtool: 'source-map',
     output: {
         path: './dist',
         filename: "[name].js"
+    },
+    module: {
+        loaders: [
+            { test: /\.css$/, loader: "style-loader!css-loader" }
+        ]
     },
     plugins: [
         new webpack.optimize.CommonsChunkPlugin({
