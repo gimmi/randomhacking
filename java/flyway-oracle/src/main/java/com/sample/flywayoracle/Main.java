bb9d7f4bfb90f4c85556dcefa9ee1f3fb307060c
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2016-09-12 14:20:56 +0200

    flyway-oracle

--- java/flyway-oracle/src/main/java/com/sample/flywayoracle/Main.java
+++ java/flyway-oracle/src/main/java/com/sample/flywayoracle/Main.java
@@ -5,16 +5,11 @@ import org.flywaydb.core.Flyway;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.sql.DriverManager;
-
 public class Main {
     private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) throws Exception {
         logger.debug("Running");
-
-        logger.info("Registering Oracle JDBC Driver");
-        DriverManager.registerDriver(new oracle.jdbc.OracleDriver());
 
         OracleDataSource dataSource = new OracleDataSource();
         dataSource.setURL("jdbc:oracle:thin:UNIT_TESTS/secret@localhost:1521:XE");
