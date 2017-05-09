95421a3cd57485518f25816547069b3fd52de217
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2017-05-09 18:27:30 +0200

    node-googleapis

--- nodejs/node-googleapis/index.js
+++ nodejs/node-googleapis/index.js
@@ -3,16 +3,16 @@ const google = require('googleapis')
 const promisify = require('./promisify')
 
 googleFacade.createAuth().then(auth => {
-  var sheets = google.sheets({ version: 'v4', auth: auth});
-  promisify(sheets.spreadsheets.values.get, {
-    spreadsheetId: '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms',
-    range: 'Class Data!A2:E',
-  }).then(function(response) {
-    console.log('Name, Major:');
-    response.values.forEach(row => {
-      console.log('%s, %s', row[0], row[4]);
+    var sheets = google.sheets({ version: 'v4', auth: auth });
+    return promisify(sheets.spreadsheets.values.get, {
+        spreadsheetId: '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms',
+        range: 'Class Data!A2:E',
+    }).then(function (response) {
+        console.log('Name, Major:');
+        response.values.forEach(row => {
+            console.log('%s, %s', row[0], row[4]);
+        })
+    }).catch(err => {
+        console.log('The API returned an error: ' + err);
     })
-  }).catch(err => {
-    console.log('The API returned an error: ' + err);
-  })
 })
