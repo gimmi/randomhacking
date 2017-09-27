a45ffa970a872eda9efeec7e5f70c8b43421e3b3
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2017-09-27 15:32:08 +0200

    SpikeCefSharp

---
+++ dotnet/SpikeCefSharp/SpikeCefSharp/MainForm.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CefSharp;
+using CefSharp.WinForms;
+
+namespace SpikeCefSharp
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var cefSettings = new CefSettings();
+
+            Cef.Initialize(cefSettings);
+
+            Controls.Add(new ChromiumWebBrowser("http://www.google.com") {
+                Dock = DockStyle.Fill
+            });
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Cef.Shutdown();
+        }
+    }
+}
