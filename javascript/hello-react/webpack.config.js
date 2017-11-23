188a0a7cc6170553fef6a3055efdcba8d2b48e9a
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2017-11-23 12:40:48 +0100

    hello-react

---
+++ javascript/hello-react/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: {
+        app: './src/app'
+    },
+    devtool: 'source-map',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js'
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            options: {
+                presets: ['env', 'react']
+            }
+        }]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html', 
+            filename: 'index.html',
+            chunks: ['app']
+        })
+    ],
+    devServer: {
+        inline: false
+    }
+}
