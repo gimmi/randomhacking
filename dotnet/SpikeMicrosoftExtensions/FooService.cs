2ff6ef6fe704f0e6e20aae6f4ad3ca7e52c7f846
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2018-02-09 11:19:36 +0100

    SpikeMicrosoftExtensions

---
+++ dotnet/SpikeMicrosoftExtensions/FooService.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace SpikeMicrosoftExtensions
+{
+    public class FooService : IFooService
+    {
+        private readonly ILogger<FooService> _logger;
+
+        public FooService(ILogger<FooService> logger)
+        {
+            _logger = logger;
+        }
+
+        public void Serve()
+        {
+            _logger.LogTrace("This is a Trace level log entry");
+            _logger.LogDebug("This is a Debug level log entry");
+            _logger.LogInformation("This is a Information level log entry");
+            _logger.LogWarning("This is a Warning level log entry");
+            _logger.LogError("This is a Error level log entry");
+            _logger.LogCritical("This is a Critical level log entry");
+        }
+    }
+}
---
+++ dotnet/SpikeMicrosoftExtensions/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace SpikeMicrosoftExtensions
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var serviceProvider = new ServiceCollection()
+                .AddLogging(builder =>
+                {
+                    // See https://msdn.microsoft.com/en-us/magazine/mt694089.aspx
+
+                    builder.SetMinimumLevel(LogLevel.Trace);
+                    builder.AddConsole();
+                })
+                .AddSingleton<IFooService, FooService>()
+                .BuildServiceProvider();
+
+            var logger = serviceProvider.GetRequiredService<ILogger<Program>>();
+            logger.LogInformation("Starting...");
+
+            var fooService = serviceProvider.GetRequiredService<IFooService>();
+
+            fooService.Serve();
+
+            serviceProvider.Dispose();
+        }
+    }
+}
