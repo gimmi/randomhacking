6e27bb0fc3410b54627a1718a4758e02a1e51382
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2018-07-28 11:21:21 +0200

    Removed hello-react

--- javascript/hello-react/src/about.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-export function About() {
-    return <h2>This is the about page</h2>
-}
--- javascript/hello-react/src/home.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-export function Home() {
-    return <h2>This is the homepage</h2>
-}
