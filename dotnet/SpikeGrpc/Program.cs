155ee108798a15cf6da5715310f03ed2f5b98481
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2018-08-01 19:58:39 +0200

    SpikeGrpc

--- dotnet/SpikeGrpc/Program.cs
+++ dotnet/SpikeGrpc/Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Spikegrpc.Proto;
 
 namespace SpikeGrpc
 {
     public static class Program
     {
-        public static void Main()
+        public static async Task Main()
         {
-            Console.WriteLine("Hello World!");
+            const int port = 50052;
+            var server = new Server {
+                Services = { EchoService.BindService(new EchoServiceServer()) },
+                Ports = { new ServerPort("localhost", port, ServerCredentials.Insecure) }
+            };
+            server.Start();
+            
+            await Console.Out.WriteLineAsync("RouteGuide server listening on port " + port);
+
+            var channel = new Channel("localhost", port, ChannelCredentials.Insecure);
+            var client = new EchoService.EchoServiceClient(channel);
+
+            var echoRequest = new EchoRequest {Message = "Hello GRPC!"};
+            var echoResponse = await client.EchoAsync(echoRequest);
+            
+            await Console.Out.WriteLineAsync($"EchoService.Echo({echoRequest.Message}) => {echoResponse.Message}");
+
+            await Console.Out.WriteLineAsync("Press enter to complete...");
+            
+            await Console.In.ReadLineAsync();
+
+            await channel.ShutdownAsync();
+            await server.ShutdownAsync();
+            
+            await Console.Out.WriteLineAsync("Done.");
         }
     }
 }
