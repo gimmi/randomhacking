87c1378015186b737a8ad2ee75d011333efc40b0
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2018-11-25 16:33:28 +0100

    SpikeNLog

---
+++ dotnet/SpikeNLog/SpikeNLog/NLogConfig.cs
@@ -0,0 +1,31 @@
+﻿using NLog;
+using NLog.Config;
+using NLog.Targets;
+using System;
+
+namespace SpikeNLog
+{
+    public static class NLogConfig
+    {
+        public static void Activate()
+        {
+            var consoleTarget = new ColoredConsoleTarget("console") {
+                Layout = "${logger} ${message}"
+            };
+
+            // See https://github.com/NLog/NLog/wiki/NLogViewer-target
+            var viewerTarget = new NLogViewerTarget("log4view") {
+                Address = "udp4://127.0.0.1:878"
+            };
+
+            var cfg = new LoggingConfiguration();
+            cfg.AddTarget(consoleTarget);
+            cfg.AddRuleForAllLevels(consoleTarget);
+
+            cfg.AddTarget(viewerTarget);
+            cfg.AddRuleForAllLevels(viewerTarget);
+
+            LogManager.Configuration = cfg;
+        }
+    }
+}
---
+++ dotnet/SpikeNLog/SpikeNLog/Program.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
+using System;
+
+namespace SpikeNLog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NLogConfig.Activate();
+
+            var serviceProvider = BuildDi();
+
+            //var loggerFactory = serviceProvider.GetRequiredService<ILoggerFactory>();
+
+            //loggerFactory.AddNLog(new NLogProviderOptions { CaptureMessageTemplates = true, CaptureMessageProperties = true });
+            //NLog.LogManager.LoadConfiguration("nlog.config");
+
+            var runner = serviceProvider.GetRequiredService<Runner>();
+            runner.Run();
+
+            Console.WriteLine("Hello World!");
+
+            Console.ReadKey();
+        }
+
+        private static ServiceProvider BuildDi()
+        {
+            return new ServiceCollection()
+                .AddLogging(builder => {
+                    builder.SetMinimumLevel(LogLevel.Trace);
+                    builder.AddNLog(new NLogProviderOptions {
+                        CaptureMessageTemplates = true,
+                        CaptureMessageProperties = true
+                    });
+                })
+                .AddTransient<Runner>()
+                .BuildServiceProvider();
+        }
+    }
+
+    public class Runner
+    {
+        private readonly ILogger<Runner> _logger;
+
+        public Runner(ILogger<Runner> logger)
+        {
+            _logger = logger;
+        }
+
+        public void Run()
+        {
+            _logger.LogTrace("This is a Trace level log entry");
+            _logger.LogDebug("This is a Debug level log entry");
+            _logger.LogInformation("This is a Information level log entry");
+            _logger.LogWarning("This is a Warning level log entry");
+            _logger.LogError("This is a Error level log entry");
+            _logger.LogCritical("This is a Critical level log entry");
+        }
+    }
+}
