0ccc14e477d5acdfa52516fa20c37a9fccf5b38e
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2019-06-24 21:28:04 +0200

    AsyncEvent

---
+++ dotnet/AsyncEvents/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsyncEvents
+{
+    public class Program
+    {
+        public static async Task Main()
+        {
+            await Console.Out.WriteLineAsync("He,l");
+            
+            var evt = new AsyncMulticastDelegate();
+            
+            await evt.InvokeAsync("x1", "x2");
+
+            evt.Add(Handler1);
+
+            await evt.InvokeAsync("y1", "y2");
+
+            evt.Add(Handler2);
+
+            await evt.InvokeAsync("z1", "z2");
+            
+            evt.Remove(Handler1);
+
+            await evt.InvokeAsync("w1", "w2");
+        }
+
+        private static async Task Handler1(string arg1, string arg2)
+        {
+            await Console.Out.WriteLineAsync($"Handler1({arg1}, {arg2})");
+        }
+
+        private static async Task Handler2(string arg1, string arg2)
+        {
+            await Console.Out.WriteLineAsync($"Handler2({arg1}, {arg2})");
+        }
+    }
+
+    public class AsyncMulticastDelegate
+    {
+        private IImmutableSet<Func<string, string, Task>> _handlers = ImmutableHashSet<Func<string, string, Task>>.Empty;
+
+        public void Add(Func<string, string, Task> handler)
+        {
+            _handlers = _handlers.Add(handler);
+        }
+
+        public void Remove(Func<string, string, Task> handler)
+        {
+            _handlers = _handlers.Remove(handler);
+        }
+
+        public Task InvokeAsync(string s1, string s2)
+        {
+            return Task.WhenAll(_handlers.Select(h => h.Invoke(s1, s2)));
+        }
+    }
+}
--- dotnet/SpikeBenchmarkDotNet/Program.cs
+++ dotnet/SpikeBenchmarkDotNet/Program.cs
@@ -1,34 +1,123 @@
 ﻿using System;
 using System.Security.Cryptography;
+using System.Text.RegularExpressions;
 using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Running;
 
 namespace SpikeBenchmarkDotNet
 {
-    public class Md5VsSha256
+    [MemoryDiagnoser, ShortRunJob]
+    public class StructVsClass
     {
         private readonly string[] _strings;
 
         private readonly SHA256 sha256 = SHA256.Create();
         private readonly MD5 md5 = MD5.Create();
 
-        public Md5VsSha256()
+        public StructVsClass()
         {
             _strings = new[] {"String 1", "String 2", "String 3", "String 4", "String 5"};
         }
+        
+        [GlobalSetup]
+        public void GlobalSetup()
+        {
+            bytes = new byte[100];
+        }
 
         [Benchmark]
-        public string Concat() => string.Concat(_strings); 
+        public StrVal Str() => new StrVal(123, 456); 
 
         [Benchmark]
-        public string Join() => string.Join("", _strings);
+        public ClassVal Cls() => new ClassVal(123, 456);
+
+        [Benchmark]
+        public StructWithArray StructWithArray()
+        { 
+            return new StructWithArray(123, );
+        }
+    }
+
+    public struct AmsAddress
+    {
+        private readonly ulong _val;
+
+        public static AmsAddress Parse(string amsAddress)
+        {
+            var match = Regex.Match(amsAddress, @"^(\d+)\.(\d+)\.(\d+)\.(\d+)\.(\d+)\.(\d+)(:(?<port>\d+))?$");
+            if (!match.Success)
+            {
+                throw new ApplicationException("Invalid AMS address: " + amsAddress);
+            }
+            
+            ulong val = (ulong) (Convert.ToByte(match.Groups[1].Value) << 56) +
+                 (ulong) (Convert.ToByte(match.Groups[2].Value) << 48) +
+                 (ulong) (Convert.ToByte(match.Groups[3].Value) << 40) +
+                 (ulong) (Convert.ToByte(match.Groups[4].Value) << 32) +
+                 (ulong) (Convert.ToByte(match.Groups[5].Value) << 24) +
+                 (ulong) (Convert.ToByte(match.Groups[6].Value) << 16);
+
+            if (match.Groups["port"].Success)
+            {
+                val += Convert.ToUInt32(match.Groups["port"].Value);
+            }
+            return new AmsAddress(val);
+        }
+
+        public AmsAddress(ulong val)
+        {
+            _val = val;
+        }
+
+        public UInt16 Port { get; private set; }
+        public byte[] ToBytes()
+        {
+            return new[] {
+                
+            }
+        }
+    }
+    public struct StructWithArray
+    {
+        public readonly int Port;
+        public readonly byte[] Bytes;
+
+        public StructWithArray(int port, byte[] bytes)
+        {
+            Port = port;
+            Bytes = bytes;
+        }
+    }
+
+    public struct StrVal
+    {
+        public StrVal(int value1, int value2)
+        {
+            Value1 = value1;
+            Value2 = value2;
+        }
+
+        public readonly int Value1;
+        public readonly int Value2;
+    }
+
+    public class ClassVal
+    {
+        public ClassVal(int value1, int value2)
+        {
+            Value1 = value1;
+            Value2 = value2;
+        }
+
+        public int Value1 { get; }
+        public int Value2 { get; }
     }
 
     public class Program
     {
         public static void Main(string[] args)
         {
-            BenchmarkRunner.Run<Md5VsSha256>();
+            BenchmarkRunner.Run<StructVsClass>();
         }
     }
 }
