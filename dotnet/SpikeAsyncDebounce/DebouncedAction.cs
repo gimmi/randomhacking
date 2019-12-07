d9c5937c3cb9b0d0b66384aa3f4ae04bccdf8fc9
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2019-12-07 14:47:26 +0100

    SpikeAsyncDebounce

---
+++ dotnet/SpikeAsyncDebounce/DebouncedAction.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SpikeAsyncDebounce
+{
+    public class DebouncedAction : IDisposable
+    {
+        private readonly TimeSpan _interval;
+        private readonly Action _action;
+
+        private CancellationTokenSource _cts;
+
+        public DebouncedAction(TimeSpan interval, Action action)
+        {
+            _interval = interval;
+            _action = action;
+        }
+
+        public void Invoke()
+        {
+            _cts?.Cancel();
+            _cts?.Dispose();
+            _cts = new CancellationTokenSource();
+            Task.Delay(_interval, _cts.Token)
+                .ContinueWith(_ => _action(), _cts.Token, TaskContinuationOptions.OnlyOnRanToCompletion, TaskScheduler.Current);
+        }
+
+        public void Dispose()
+        {
+            _cts?.Cancel();
+            _cts?.Dispose();
+        }
+    }
+}
---
+++ dotnet/SpikeAsyncDebounce/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SpikeAsyncDebounce
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            using var action = new DebouncedAction(TimeSpan.FromSeconds(2), async () => {
+                await Console.Out.WriteLineAsync("Invoked!");
+            });
+
+            Console.WriteLine("Press any key to invoke...");
+
+            while (Console.ReadKey().Key != ConsoleKey.Escape)
+            {
+                action.Invoke();
+            }
+        }
+    }
+}
