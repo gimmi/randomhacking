eb939a79f65d40f014ce615093bb1ee69004db4e
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2019-12-11 15:09:06 +0100

    SpikeAsyncDebounce

--- dotnet/SpikeAsyncDebounce/Program.cs
+++ dotnet/SpikeAsyncDebounce/Program.cs
@@ -1,11 +1,14 @@
 ﻿using System;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace SpikeAsyncDebounce
 {
     public class Program
     {
+/*
         public static void Main()
         {
             var action = new DebouncedAction(TimeSpan.FromSeconds(2), async () => {
@@ -23,6 +26,65 @@ namespace SpikeAsyncDebounce
             Console.Out.WriteLine();
             Console.Out.WriteLine("Disposing");
             action.Dispose();
+        }
+*/
+
+        [Test]
+        public async Task Should_invoke_as_expected()
+        {
+            var count = 0;
+            var sut = new DebouncedAction(TimeSpan.FromSeconds(.5), () => Interlocked.Increment(ref count));
+
+            await Task.Delay(1_000);
+            Assert.That(count, Is.EqualTo(0));
+
+            sut.Invoke();
+
+            await Task.Delay(200);
+            Assert.That(count, Is.EqualTo(0));
+
+            await Task.Delay(500);
+            Assert.That(count, Is.EqualTo(1));
+
+            await Task.Delay(1_000);
+            Assert.That(count, Is.EqualTo(1));
+
+            sut.Invoke();
+
+            await Task.Delay(250);
+            Assert.That(count, Is.EqualTo(1));
+
+            sut.Invoke();
+
+            await Task.Delay(250);
+            Assert.That(count, Is.EqualTo(1));
+
+            await Task.Delay(500);
+            Assert.That(count, Is.EqualTo(2));
+
+            sut.Dispose();
+        }
+
+        [Test]
+        public async Task Should_tolerate_concurrent_invoke_calls()
+        {
+            var count = 0;
+            var sut = new DebouncedAction(TimeSpan.FromSeconds(.5), () => Interlocked.Increment(ref count));
+
+            await Task.WhenAll(Enumerable.Range(0, 100).Select(_ => Task.Run(() => {
+                for (var i = 0; i < 1_000; i++)
+                {
+                    sut.Invoke();
+                }
+            })));
+
+            Assert.That(count, Is.EqualTo(0));
+
+            await Task.Delay(1_000);
+
+            Assert.That(count, Is.EqualTo(1));
+
+            sut.Dispose();
         }
     }
 }
