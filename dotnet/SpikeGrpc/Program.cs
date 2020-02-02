37ce7bd6635ecd76f1b40a920321f8d243d883b4
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2020-02-02 11:23:32 +0100

    SpikeGrpc

--- dotnet/SpikeGrpc/Program.cs
+++ dotnet/SpikeGrpc/Program.cs
@@ -22,57 +22,9 @@ namespace SpikeGrpc
         {
             var host = new HostBuilder()
                 .UseEnvironment(Environments.Development)
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .ConfigureHostConfiguration(config => {
-                    config.AddEnvironmentVariables(prefix: "DOTNET_");
-                    if (args != null)
-                    {
-                        config.AddCommandLine(args);
-                    }
-                })
-                .ConfigureAppConfiguration((hostingContext, config) => {
-                    var env = hostingContext.HostingEnvironment;
-
-                    config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-                        .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true);
-
-                    if (env.IsDevelopment() && !string.IsNullOrEmpty(env.ApplicationName))
-                    {
-                        var appAssembly = Assembly.Load(new AssemblyName(env.ApplicationName));
-                        if (appAssembly != null)
-                        {
-                            config.AddUserSecrets(appAssembly, optional: true);
-                        }
-                    }
-
-                    config.AddEnvironmentVariables();
-
-                    if (args != null)
-                    {
-                        config.AddCommandLine(args);
-                    }
-                })
-                .ConfigureLogging((hostingContext, logging) => {
-                    var isWindows = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
-
-                    // IMPORTANT: This needs to be added *before* configuration is loaded, this lets
-                    // the defaults be overridden by the configuration.
-                    if (isWindows)
-                    {
-                        // Default the EventLogLoggerProvider to warning or above
-                        logging.AddFilter<EventLogLoggerProvider>(level => level >= LogLevel.Warning);
-                    }
-
-                    logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                .ConfigureLogging(logging => {
+                    logging.SetMinimumLevel(LogLevel.Trace);
                     logging.AddConsole();
-                    logging.AddDebug();
-                    logging.AddEventSourceLogger();
-
-                    if (isWindows)
-                    {
-                        // Add the EventLogLoggerProvider on windows machines
-                        logging.AddEventLog();
-                    }
                 })
                 .UseDefaultServiceProvider((context, options) => {
                     var isDevelopment = context.HostingEnvironment.IsDevelopment();
