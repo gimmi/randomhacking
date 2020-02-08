a06a7fce80196de86dbf6445d45d11e6b879a056
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2020-02-08 12:54:21 +0100

    SpikeNamedPipe

---
+++ dotnet/SpikeNamedPipe/ClientPipe/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO.Pipes;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ClientPipe
+{
+    public static class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            await OutAsync("Starting client");
+
+            var ctrlC = BindCtrlC();
+
+            while (!ctrlC.IsCancellationRequested)
+            {
+                try
+                {
+                    await OutAsync("Connecting");
+
+                    await using var pipeClient = new NamedPipeClientStream(".", "testpipe", PipeDirection.InOut, PipeOptions.Asynchronous);
+                    await pipeClient.ConnectAsync(ctrlC);
+
+                    var value = 0;
+
+                    while (!ctrlC.IsCancellationRequested)
+                    {
+                        var buffer = BitConverter.GetBytes(++value);
+                        await pipeClient.WriteAsync(buffer, 0, buffer.Length, ctrlC);
+
+                        await OutAsync("Write: " + buffer.Length);
+
+                        await Task.Delay(1_000, ctrlC);
+                    }
+                }
+                catch (TaskCanceledException e) when (e.CancellationToken == ctrlC)
+                {
+                    await OutAsync("Ctrl-C");
+                }
+                catch (Exception e)
+                {
+                    await OutAsync(e);
+                }
+            }
+        }
+
+        public static Task OutAsync(object msg) => Console.Out.WriteLineAsync("CLI - " + msg);
+
+        public static CancellationToken BindCtrlC()
+        {
+            var stopCts = new CancellationTokenSource();
+            Console.CancelKeyPress += (s, e) => {
+                e.Cancel = true;
+                stopCts.Cancel();
+            };
+            return stopCts.Token;
+        }
+    }
+}
---
+++ dotnet/SpikeNamedPipe/ServerPipe/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ServerPipe
+{
+    public static class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            await OutAsync("Starting");
+
+            var ctrlC = BindCtrlC();
+
+            while (!ctrlC.IsCancellationRequested)
+            {
+                try
+                {
+                    await OutAsync("Listening for connection");
+                    await using var pipeServer = new NamedPipeServerStream("testpipe", PipeDirection.InOut, 1, PipeTransmissionMode.Byte, PipeOptions.Asynchronous);
+                    await pipeServer.WaitForConnectionAsync(ctrlC);
+
+                    while (!ctrlC.IsCancellationRequested)
+                    {
+                        var buffer = new byte[1_024];
+
+                        await OutAsync("Waiting for data");
+                        var length = await pipeServer.ReadAsync(buffer, 0, buffer.Length, ctrlC);
+
+                        await OutAsync("Received: " + BitConverter.ToString(buffer, 0, length));
+                    }
+                    pipeServer.Disconnect();
+                }
+                catch (TaskCanceledException e) when (e.CancellationToken == ctrlC)
+                {
+                    await OutAsync("Ctrl-C");
+                }
+                catch (Exception e)
+                {
+                    await OutAsync(e);
+                }
+            }
+
+            await OutAsync("Stopping server");
+        }
+
+        public static Task OutAsync(object msg) => Console.Out.WriteLineAsync("SRV - " + msg);
+
+        public static CancellationToken BindCtrlC()
+        {
+            var stopCts = new CancellationTokenSource();
+            Console.CancelKeyPress += (s, e) => {
+                e.Cancel = true;
+                stopCts.Cancel();
+            };
+            return stopCts.Token;
+        }
+    }
+}
