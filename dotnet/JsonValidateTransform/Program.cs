566bc85ff52f22d43259a9d893d57ff665804b60
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2020-04-12 10:17:51 +0200

    JsonValidateTransform

---
+++ dotnet/JsonValidateTransform/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using Cocona;
+using DotLiquid;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Schema;
+
+namespace JsonValidateTransform
+{
+    public class Program
+    {
+        public static void Main(string[] args) => CoconaApp.Run<Program>(args);
+
+        public async Task RunAsync([Option("in")] string inputDir, [Option("out")] string outputDir)
+        {
+            var modelPath = Path.Combine(inputDir, "model.json");
+            var modelJson = await File.ReadAllTextAsync(modelPath);
+            var model = JObject.Parse(modelJson);
+
+            var schemaPath = Path.Combine(inputDir, "schema.json");
+            var schemaJson = await File.ReadAllTextAsync(schemaPath);
+            var schema = JSchema.Parse(schemaJson);
+
+            if (!model.IsValid(schema))
+            {
+                throw new Exception("Model invalid according to schema");
+            }
+
+            Directory.CreateDirectory(outputDir);
+            foreach (var templatePath in Directory.GetFiles(inputDir, "*.liquid"))
+            {
+                var outputPath = Path.Combine(outputDir, Path.GetFileNameWithoutExtension(templatePath) + ".json");
+                await Console.Out.WriteLineAsync($"{Path.GetFileName(templatePath)} -> {Path.GetFileName(outputPath)}");
+                var templateText = await File.ReadAllTextAsync(templatePath);
+                var template = Template.Parse(templateText);
+
+                var modelDict = model.ToObject<IDictionary<string, object>>();
+                var modelHash = Hash.FromDictionary(modelDict);
+
+                var outputRaw = template.Render(modelHash);
+                var outputJson = JObject.Parse(outputRaw).ToString(Formatting.Indented);
+                await File.WriteAllTextAsync(outputPath, outputJson);
+            }
+        }
+    }
+}
