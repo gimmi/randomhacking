156e9108ed794d76b88f2c6736fcc53e5aed65a5
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2020-04-26 09:29:57 +0200

    node-tail

--- nodejs/node-tail/server/src/app.js
+++ nodejs/node-tail/server/src/app.js
@@ -8,14 +8,26 @@ const fluentd = require('./fluentd-forward');
 
 // Debug stuff -----------------------------------------------------------------
 if (debug.enabled) {
+    const rndFn = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+
     debug('Enabling debug stuff')
 
     const containerNames = [ 'container-one', 'container-two', 'container-three', 'container-four' ]
 
+    const sampleTexts = [
+        '<2>ConsoleApp.Program[10] Critical message #',
+        '<3>ConsoleApp.Program[10] Error message #',
+        '<4>ConsoleApp.Program[10] Warning message #',
+        '<6>ConsoleApp.Program[10] Information message #',
+        '<7>ConsoleApp.Program[10] Debug message #',
+    ]
+
     let counter = 0;
     setInterval(() => {
         counter += 1;
-        bus.emit('message', { container_name: containerNames[counter % 4], log: 'Log #' + counter })
+        const containerName = containerNames[rndFn(0, 3)]
+        const sampleText = sampleTexts[rndFn(0, 4)]
+        bus.emit('message', { container_name: containerName, log: sampleText + counter })
     }, 1000);
 
     bus.on('message', msg => debug('Published:', msg));
