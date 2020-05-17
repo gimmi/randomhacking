b5d2db0874b22a86a65737e19e6d54fda50d7253
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2020-05-17 10:49:51 +0200

    JsonMerge

---
+++ dotnet/JsonMerge/JsonMerger.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+
+namespace JsonMerge
+{
+    public static class JsonMerger
+    {
+        public static JToken Merge(JToken first, JToken second)
+        {
+            if (first is JArray firstArray && second is JArray secondArray)
+            {
+                var resultArray = new JArray();
+                MergeArray(resultArray, firstArray);
+                MergeArray(resultArray, secondArray);
+                return resultArray;
+            }
+
+            if (first is JObject firstObject && second is JObject secondObject)
+            {
+                var resultObject = new JObject();
+                MergeObject(resultObject, firstObject);
+                MergeObject(resultObject, secondObject);
+                return resultObject;
+            }
+
+            if (first.Type == JTokenType.Null)
+            {
+                return second;
+            }
+
+            return first;
+        }
+
+        private static void MergeArray(JArray destArray, JArray newArray)
+        {
+            foreach (var newToken in newArray)
+            {
+                var append = true;
+                var newId = GetTokenId(newToken);
+                for (var idx = 0; idx < destArray.Count; idx++)
+                {
+                    var destToken = destArray[idx];
+                    var destId = GetTokenId(destToken);
+                    if (JToken.DeepEquals(destId, newId))
+                    {
+                        destArray[idx] = Merge(destToken, newToken);
+                        append = false;
+                        break;
+                    }
+                }
+
+                if (append)
+                {
+                    destArray.Add(newToken);
+                }
+            }
+        }
+
+        private static void MergeObject(JObject destObject, JObject newObject)
+        {
+            foreach (var (newId, newToken) in newObject)
+            {
+                if (destObject.TryGetValue(newId, out var destToken))
+                {
+                    destObject[newId] = Merge(destToken, newToken);
+                }
+                else
+                {
+                    destObject[newId] = newToken;
+                }
+            }
+        }
+
+        private static JToken GetTokenId(JToken token)
+        {
+            if (token is JObject obj)
+            {
+                var sc = StringComparison.OrdinalIgnoreCase;
+                obj.TryGetValue("id", sc, out var valueToken);
+                obj.TryGetValue("$id", sc, out valueToken);
+                return valueToken;
+            }
+
+            return token;
+        }
+    }
+}
---
+++ dotnet/JsonMerge/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace JsonMerge
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var firstToken = JToken.Parse(@"{
+                a: 1,
+                b: null,
+                raw_ary: ['a', 'b', 3],
+                obj: {
+                    a: 1,
+                    b: 2,
+                    c: 3
+                },
+                obj_ary: [{
+                    $id: '1',
+                    a: 1,
+                    b: 2
+                }, {
+                    $id: '2',
+                    a: 3,
+                    b: 4
+                }]
+            }");
+            var secondToken = JToken.Parse(@"{
+                a: 3,
+                b: 2,
+                raw_ary: ['a', 4, 'b', 3],
+                obj: {
+                    a: 1,
+                    b: 'xoxo',
+                    d: 4
+                },
+                obj_ary: [{
+                    $id: '1',
+                    a: 'xoxo',
+                    b: 'xoxo',
+                    c: 'lala'
+                }, {
+                    $id: '4',
+                    a: 'ca',
+                    b: 'so'
+                }]
+            }");
+
+            var resultToken = JsonMerger.Merge(firstToken, secondToken);
+
+            var resultJson = resultToken.ToString(Formatting.Indented);
+
+            Console.WriteLine(resultJson);
+        }
+    }
+}
