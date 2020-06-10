35ac12614ef4db3487cd799fac3c3ad8eb8ce49a
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2020-06-10 21:01:07 +0200

    SpikeJint

--- dotnet/SpikeJint/Program.cs
+++ dotnet/SpikeJint/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Text;
 using Jint;
 using Jint.Native;
 
@@ -8,6 +10,19 @@ namespace SpikeJint
     {
         public static void Main()
         {
+            var baseDir = Path.Combine(Directory.GetCurrentDirectory(), "test_dir");
+            if (Directory.Exists(baseDir))
+            {
+                Directory.Delete(baseDir, true);
+            }
+            Directory.CreateDirectory(baseDir);
+            File.WriteAllText(Path.Combine(baseDir, "file.json"), "{ a:1, b:2 /* comment */ }");
+            File.WriteAllText(Path.Combine(baseDir, "file.js"), @"
+                function prefix(val) {
+                    return 'my' + val;
+                }
+            ");
+
             var input = @"{
                 'aString': 'value',
                 'aNumber': 3.14,
@@ -24,30 +39,64 @@ namespace SpikeJint
             }".Replace("'", "\"");
 
             var script = @"
+                load('file.js');
+                var file = load('file.json');
+
                 output = {
                     myString: input.aString,
-                    myArray: input.anArray.map(prefix)
-                }
-
-                function prefix(val) {
-                    return 'my' + val;
-                }
+                    myArray: input.anArray.map(prefix),
+                    file: file
+                }                
             ";
 
-            var output = TransformJson(script, input);
+            var output = TransformJson(script, input, baseDir);
 
             Console.Out.WriteLine(output);
         }
 
-        private static string TransformJson(string script, string input)
+        private static string TransformJson(string script, string input, string baseDir)
         {
             var engine = new Engine();
 
-            var inputJsValue = engine.Json.Parse(Undefined.Instance, new JsValue[] {engine.String.Construct(input)});
+            var jsApi = new JsApi(engine, baseDir);
 
-            var outputJsValue = engine.SetValue("input", inputJsValue).Execute(script).GetValue("output");
+            var inputJsValue = engine.Json.Parse(Undefined.Instance, new JsValue[] {engine.String.Construct(input)});
+            var outputJsValue = engine
+                .SetValue("load", new Func<JsValue, JsValue>(jsApi.Load))
+                .SetValue("input", inputJsValue)
+                .Execute(script)
+                .GetValue("output");
 
             return engine.Json.Stringify(Undefined.Instance, new[] {outputJsValue, Undefined.Instance, engine.Number.Construct(2)}).ToString();
+        }
+
+        public class JsApi
+        {
+            private readonly Engine _engine;
+            private readonly string _baseDir;
+
+            public JsApi(Engine engine, string baseDir)
+            {
+                _engine = engine;
+                _baseDir = baseDir;
+            }
+
+            public JsValue Load(JsValue value)
+            {
+                var path = value.AsString();
+                if (!Path.IsPathRooted(path))
+                {
+                    path = Path.GetFullPath(path, _baseDir);
+                }
+                Console.Out.WriteLine("Loading " + path);
+                var content = File.ReadAllText(path, Encoding.UTF8);
+                if (Path.GetExtension(path) == ".json")
+                {
+                    // See https://stackoverflow.com/a/45016414/66629
+                    content = string.Concat("(", content, ")");
+                }
+                return _engine.Execute(content).GetCompletionValue();
+            }
         }
     }
 }
