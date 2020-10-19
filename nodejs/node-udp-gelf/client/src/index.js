212632c0549a218ed7022e08092da9ee6475dea4
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2020-10-19 21:05:15 +0200

    node-udp-gelf moved to https://github.com/gimmi/gelf2azure

--- nodejs/node-udp-gelf/client/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import { MainComponent } from './MainComponent';
-
-ReactDOM.render(<MainComponent />, document.getElementById('root'));
--- nodejs/node-udp-gelf/server/src/fetch.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const fetch = require('node-fetch')
-const ProxyAgent = require('proxy-agent')
-
-module.exports = (url, options) => {
-  const agent = new ProxyAgent()
-  return fetch(url, { agent, ...options })
-}
