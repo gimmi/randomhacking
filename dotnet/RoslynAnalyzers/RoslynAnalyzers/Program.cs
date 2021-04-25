924df02dd3f844c402028ba2d1bc28b171131848
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2021-04-25 15:41:03 +0200

    RoslynAnalyzers

---
+++ dotnet/RoslynAnalyzers/RoslynAnalyzers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace RoslynAnalyzers
+{
+    public static class Program
+    {
+        private static ValueTask _valueTask;
+
+        public static async Task Main()
+        {
+            _valueTask = new MyAsyncDisposable().DisposeAsync();
+
+            await Console.Out.WriteLineAsync("Hello World!");
+        }
+    }
+
+    public class MyAsyncDisposable : IAsyncDisposable
+    {
+        public ValueTask DisposeAsync() => ValueTask.CompletedTask;
+    }
+}
